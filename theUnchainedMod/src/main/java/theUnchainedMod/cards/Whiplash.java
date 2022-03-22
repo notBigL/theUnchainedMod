efe4385c4beafc5be198fe0eac1e0cabf1c27220
18penning <8penning@informatik.uni-hamburg.de>
2022-03-22 13:23:13 +0100

    Added Whiplash and its Power and changed the Card and Power description

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Whiplash.java
@@ -0,0 +1,50 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.WhiplashPower;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class Whiplash extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(Whiplash.class.getSimpleName());
+    public static final String IMG = makeCardPath("Whiplash.png");
+    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 4;
+    private static final int CHAIN_LENGTH = 2;
+    private static final int MAGIC_NUMBER = 12;
+
+    public Whiplash() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeBaseCost(0);
+        }
+
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new WhiplashPower(p, CHAIN_LENGTH, m, this.magicNumber, TYPE)));
+    }
+}
---
+++ theUnchainedMod/src/main/java/theUnchainedMod/powers/WhiplashPower.java
@@ -0,0 +1,39 @@
+package theUnchainedMod.powers;
+
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import theUnchainedMod.DefaultMod;
+
+public class WhiplashPower extends AbstractChainPower {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = DefaultMod.makeID("WhiplashPower");
+    private static final PowerStrings powerstrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerstrings.NAME;
+    public static final String[] DESCRIPTIONS = powerstrings.DESCRIPTIONS;
+    public static AbstractPower.PowerType POWER_TYPE = PowerType.BUFF;
+    private final int chainDamage;
+
+
+    public WhiplashPower(AbstractCreature owner, int amount, AbstractMonster monster, int damageAmount, AbstractCard.CardType cardType) {
+        super(POWER_ID, owner, amount, new DamageAction(monster, new DamageInfo(owner, damageAmount, DamageInfo.DamageType.HP_LOSS)), cardType);
+        this.name = NAME;
+        this.type = POWER_TYPE;
+        chainDamage = damageAmount;
+        this.updateDescription();
+    }
+
+    public void updateDescription() {
+        if (this.amount == 1) {
+            this.description = DESCRIPTIONS[0] + this.chainDamage + DESCRIPTIONS[1];
+        } else {
+            this.description = DESCRIPTIONS[2] + this.amount + DESCRIPTIONS[3] + this.chainDamage + DESCRIPTIONS[1];
+        }
+    }
+}
