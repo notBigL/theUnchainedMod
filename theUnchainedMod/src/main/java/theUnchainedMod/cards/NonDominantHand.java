8b2ee44d1d33a02cbbb39eec2874f6bec2c1774b
18penning <8penning@informatik.uni-hamburg.de>
2022-03-22 12:16:53 +0100

    Deleted useless imports

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/NonDominantHand.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/NonDominantHand.java
@@ -1,9 +1,5 @@
 package theUnchainedMod.cards;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
