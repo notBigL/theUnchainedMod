e9732ecbeb0c52f0480ddbdfd915fe5f33ad0acd
18penning <8penning@informatik.uni-hamburg.de>
2022-03-22 20:42:49 +0100

    Added Cruiserweight and its Power

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Cruiserweight.java
@@ -0,0 +1,44 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.CruiserweightPower;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class Cruiserweight extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(Cruiserweight.class.getSimpleName());
+    public static final String IMG = makeCardPath("Cruiserweight.png");
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 2;
+    private static final int MAGIC_NUMBER = 1;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
+
+    public Cruiserweight() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
+        }
+
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new CruiserweightPower(p, p, this.magicNumber)));
+    }
+}
