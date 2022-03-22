a825cba5c335b38f3d4323e2181892525f6afff2
18penning <8penning@informatik.uni-hamburg.de>
2022-03-22 12:44:30 +0100

    Added the card MorningStar

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/MorningStar.java
@@ -0,0 +1,61 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ReduceCostAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class MorningStar extends AbstractDynamicCard {
+    public static final String ID = DefaultMod.makeID(MorningStar.class.getSimpleName());
+    public static final String IMG = makeCardPath("MorningStar.png");
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 4;
+    private static final int DAMAGE = 25;
+    private static final int UPGRADE_PLUS_DMG = 5;
+    private static final int MAGIC_NUMBER = 15;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 5;
+
+    public MorningStar() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        this.isMultiDamage = true;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.magicNumber, DamageInfo.DamageType.HP_LOSS), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        if (this.cost == 2) {
+            this.addToBot(new ReduceCostAction(this.uuid, 1));
+        }
+    }
+
+    @Override
+    public void onPlayCard(AbstractCard c, AbstractMonster m) {
+        if (c.cost == 2 && !c.uuid.equals(this.uuid)) {
+            this.updateCost(-1);
+        }
+    }
+}
