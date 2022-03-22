9bbb8a4164fa49985ef66afda59872d42d6e87e0
18penning <8penning@informatik.uni-hamburg.de>
2022-03-22 20:31:45 +0100

    Added ClenchTeeth and its Power

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/ClenchTeeth.java
@@ -0,0 +1,44 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.ClenchTeethPower;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class ClenchTeeth extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(ClenchTeeth.class.getSimpleName());
+    public static final String IMG = makeCardPath("ClenchTeeth.png");
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int MAGIC_NUMBER = 5;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 2;
+
+    public ClenchTeeth() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
+        }
+
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new ClenchTeethPower(p, p, this.magicNumber)));
+    }
+}
