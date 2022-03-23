aaf1ea1c8aa0082c47e02f4888a07342fd133382
18penning <8penning@informatik.uni-hamburg.de>
2022-03-23 23:56:34 +0100

    Added the Card PlayDead and its Power. Added the secondMagicNumber to it and the Description of NonDominantHand.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/PlayDead.java
@@ -0,0 +1,48 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.PlayDeadPower;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class PlayDead extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(PlayDead.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlayDead.png");
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 0;
+    private static final int MAGIC_NUMBER = 2;
+    private static final int SECOND_MAGIC_NUMBER = 3;
+    private static final int UPGRADE_PLUS_SECOND_MAGIC_NUMBER = -1;
+
+
+    public PlayDead() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeDefaultSecondMagicNumber(UPGRADE_PLUS_SECOND_MAGIC_NUMBER);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new VulnerablePower(p, this.magicNumber, false)));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new PlayDeadPower(p, defaultSecondMagicNumber, TYPE)));
+    }
+}
---
+++ theUnchainedMod/src/main/java/theUnchainedMod/powers/PlayDeadPower.java
@@ -0,0 +1,37 @@
+package theUnchainedMod.powers;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.IntangiblePlayerPower;
+import theUnchainedMod.DefaultMod;
+
+public class PlayDeadPower extends AbstractChainPower {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = DefaultMod.makeID("PlayDeadPower");
+    private static final PowerStrings powerstrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerstrings.NAME;
+    public static final String[] DESCRIPTIONS = powerstrings.DESCRIPTIONS;
+    public static AbstractPower.PowerType POWER_TYPE = PowerType.BUFF;
+
+
+    public PlayDeadPower(AbstractCreature owner, int amount, AbstractCard.CardType cardType) {
+        super(POWER_ID, owner, amount, new ApplyPowerAction(owner, owner, new IntangiblePlayerPower(owner, 1)), cardType);
+        this.name = NAME;
+        this.type = POWER_TYPE;
+        this.updateDescription();
+    }
+
+    public void updateDescription() {
+        if (this.amount == 1) {
+            this.description = DESCRIPTIONS[0];
+        } else {
+            this.description = DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[2];
+        }
+    }
+}
