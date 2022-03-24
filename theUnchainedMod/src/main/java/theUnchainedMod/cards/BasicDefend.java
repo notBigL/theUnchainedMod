342face5ef7aebfde52666a7b6939579e4d06e1b
18penning <8penning@informatik.uni-hamburg.de>
2022-03-24 21:07:12 +0100

    Added a BasicStrike and a BasicDefend

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/BasicDefend.java
@@ -0,0 +1,46 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.UpgradeRandomCardAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class BasicDefend extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(BasicDefend.class.getSimpleName());
+    public static final String IMG = makeCardPath("BasicDefend.png");
+    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 5;
+    private static final int UPGRADE_PLUS_BLOCK = 3;
+
+    public BasicDefend() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = block = BLOCK;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, this.block));
+    }
+}
