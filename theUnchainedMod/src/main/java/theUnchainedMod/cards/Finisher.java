c24147cbc6b14eb25634fdc5e4f2bc16758ac11a
18penning <8penning@informatik.uni-hamburg.de>
2022-03-25 00:16:13 +0100

    Added the Card Finisher

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Finisher.java
@@ -0,0 +1,57 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+
+import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class Finisher extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(Finisher.class.getSimpleName());
+    public static final String IMG = makeCardPath("Finisher.png");
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 2;
+    private static final int DAMAGE = 30;
+
+    public Finisher() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        this.isMultiDamage = true;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+    }
+
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        for (AbstractPower aP : p.powers) {
+            if (aP.name.equals("Attack Chain") && aP.amount == 1) {
+                return true;
+            }
+        }
+        this.cantUseMessage = languagePack.getCardStrings(ID).EXTENDED_DESCRIPTION[0];
+        return false;
+    }
+}
