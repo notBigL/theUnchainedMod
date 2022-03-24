15bf48fa8fe2dbdc09060195936333f81c971ee1
18penning <8penning@informatik.uni-hamburg.de>
2022-03-24 17:20:06 +0100

    Added the MakeRoom Card and its Power

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/MakeRoom.java
@@ -0,0 +1,57 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.MakeRoomPower;
+import theUnchainedMod.powers.RelayPower;
+
+import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class MakeRoom extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(MakeRoom.class.getSimpleName());
+    public static final String IMG = makeCardPath("MakeRoom.png");
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+    public static final String UPGRADE_DESCRIPTION = languagePack.getCardStrings(ID).UPGRADE_DESCRIPTION;
+
+    private static final int COST = 2;
+    private static final int MAGIC_NUMBER = 15;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 2;
+    private static final int SECOND_MAGIC_NUMBER = 2;
+    private static final int ENERGYGAIN = 2;
+    private int energyGain;
+
+
+
+    public MakeRoom() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
+        this.energyGain = ENERGYGAIN;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
+            this.energyGain++;
+            this.rawDescription = UPGRADE_DESCRIPTION;
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new RelayPower(p, p, this.magicNumber)));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new MakeRoomPower(p, defaultSecondMagicNumber, new GainEnergyAction(this.energyGain), TYPE)));
+    }
+}
