d2243b85436d087b645016eba64a9af56cb69705
18penning <8penning@informatik.uni-hamburg.de>
2022-03-24 16:23:53 +0100

    Added the RecklessSwing Card

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/RecklessSwing.java
@@ -0,0 +1,51 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.RelayedDamagePower;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class RecklessSwing extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(RecklessSwing.class.getSimpleName());
+    public static final String IMG = makeCardPath("RecklessSwing.png");
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF_AND_ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 13;
+    private static final int UPGRADE_PLUS_DMG = 4;
+    private static final int MAGIC_NUMBER = 4;
+
+    public RecklessSwing() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+        }
+
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new RelayedDamagePower(p, p, magicNumber)));
+    }
+}
