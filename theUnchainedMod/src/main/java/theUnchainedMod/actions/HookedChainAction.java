b6fa75b778cdee9f452b19b70f38b034e1c56d50
18penning <8penning@informatik.uni-hamburg.de>
2022-03-26 15:02:33 +0100

    added the Card HookedChain and its Action.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/HookedChainAction.java
@@ -0,0 +1,43 @@
+package theUnchainedMod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
+
+public class HookedChainAction extends AbstractGameAction {
+
+    private DamageInfo info;
+    private int amount;
+    private AbstractPlayer player;
+
+
+    public HookedChainAction(AbstractCreature target, DamageInfo info, int vulnAmount, AbstractPlayer p) {
+        this.info = info;
+        this.setValues(target, info);
+        this.actionType = ActionType.DAMAGE;
+        this.duration = Settings.ACTION_DUR_MED;
+        this.amount = vulnAmount;
+        this.player = p;
+    }
+
+    public void update() {
+        if (this.duration == Settings.ACTION_DUR_MED && this.target != null) {
+            AbstractDungeon.effectList.add(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, AttackEffect.NONE));
+            this.target.damage(this.info);
+            if (!((((AbstractMonster) this.target).isDying || this.target.currentHealth <= 0) && !this.target.halfDead)) {
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(player, player, new VulnerablePower(player, amount, false)));
+            }
+            if (AbstractDungeon.getCurrRoom().monsters.areMonstersBasicallyDead()) {
+                AbstractDungeon.actionManager.clearPostCombatActions();
+            }
+        }
+        this.tickDuration();
+    }
+}
---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/HookedChain.java
@@ -0,0 +1,49 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.HookedChainAction;
+import theUnchainedMod.characters.TheDefault;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class HookedChain extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(HookedChain.class.getSimpleName());
+    public static final String IMG = makeCardPath("HookedChain.png");
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 14;
+    private static final int UPGRADE_PLUS_DMG = 6;
+    private static final int MAGIC_NUMBER = 2;
+
+    public HookedChain() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+        }
+
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new HookedChainAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), magicNumber, p));
+    }
+}
