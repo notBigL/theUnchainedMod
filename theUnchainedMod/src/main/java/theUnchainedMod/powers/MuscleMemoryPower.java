8b7338910d4467d950af6a6254fa50e975c34877
18penning <8penning@informatik.uni-hamburg.de>
2022-03-26 13:52:59 +0100

    added the Card MuscleMemory and its Power.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/powers/MuscleMemoryPower.java
@@ -0,0 +1,87 @@
+package theUnchainedMod.powers;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.util.TextureLoader;
+
+public class MuscleMemoryPower extends AbstractPower {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = DefaultMod.makeID("MuscleMemoryPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    private static final Texture texture48 = TextureLoader.getTexture("theUnchainedModResources/images/powers/MuscleMemoryPower_power48.png");
+    private static final Texture texture128 = TextureLoader.getTexture("theUnchainedModResources/images/powers/MuscleMemoryPower_power128.png");
+
+    public MuscleMemoryPower(final AbstractCreature owner, final AbstractCreature source, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        this.source = source;
+        type = PowerType.BUFF;
+        isTurnBased = false;
+
+        this.region128 = new TextureAtlas.AtlasRegion(texture128, 0, 0, 128, 128);
+        this.region48 = new TextureAtlas.AtlasRegion(texture48, 0, 0, 48, 48);
+
+        updateDescription();
+    }
+
+    public void updateDescription() {
+        if (this.amount == 1) {
+            this.description = DESCRIPTIONS[0];
+        } else {
+            this.description = DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[2];
+        }
+    }
+
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (!card.purgeOnUse && card.cost == 2 && this.amount > 0) {
+            this.flash();
+            AbstractMonster m = null;
+            if (action.target != null) {
+                m = (AbstractMonster)action.target;
+            }
+
+            AbstractCard tmp = card.makeSameInstanceOf();
+            AbstractDungeon.player.limbo.addToBottom(tmp);
+            tmp.current_x = card.current_x;
+            tmp.current_y = card.current_y;
+            tmp.target_x = (float) Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
+            tmp.target_y = (float)Settings.HEIGHT / 2.0F;
+            if (m != null) {
+                tmp.calculateCardDamage(m);
+            }
+
+            tmp.purgeOnUse = true;
+            AbstractDungeon.actionManager.addCardQueueItem(new CardQueueItem(tmp, m, card.energyOnUse, true, true), true);
+            --this.amount;
+            if (this.amount == 0) {
+                AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+            }
+        }
+
+    }
+
+    public void atEndOfTurn(boolean isPlayer) {
+        if (isPlayer) {
+            AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+        }
+    }
+}
