9b1c1d49b52fd52d50bb7806965fb8d38aab1171
18penning <8penning@informatik.uni-hamburg.de>
2022-03-26 12:57:25 +0100

    Deleted useless imports

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/Cower.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Cower.java
@@ -6,7 +6,6 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.FrailPower;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
 import theUnchainedMod.DefaultMod;
 import theUnchainedMod.actions.AllEnemiesVulnerableAction;
 import theUnchainedMod.characters.TheDefault;
