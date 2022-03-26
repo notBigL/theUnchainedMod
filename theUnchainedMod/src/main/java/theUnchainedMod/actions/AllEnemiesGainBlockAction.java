dd59093b1c7b5f919eb96e6e285e6855571e3b2c
18penning <8penning@informatik.uni-hamburg.de>
2022-03-26 13:23:40 +0100

    added the Card KeepDistance and its Action

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/AllEnemiesGainBlockAction.java
@@ -0,0 +1,25 @@
+package theUnchainedMod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class AllEnemiesGainBlockAction extends AbstractGameAction {
+
+
+    public AllEnemiesGainBlockAction(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public void update() {
+        {
+            for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
+                AbstractDungeon.actionManager.addToBottom(new GainBlockAction(mo, this.amount));
+            }
+        }
+        this.isDone = true;
+    }
+}
+
---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/KeepDistance.java
@@ -0,0 +1,48 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.AllEnemiesGainBlockAction;
+import theUnchainedMod.characters.TheDefault;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class KeepDistance extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(KeepDistance.class.getSimpleName());
+    public static final String IMG = makeCardPath("KeepDistance.png");
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 10;
+    private static final int UPGRADE_PLUS_BLOCK = 2;
+    private static final int MAGIC_NUMBER = 5;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = -1;
+
+    public KeepDistance() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = block = BLOCK;
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, this.block));
+        AbstractDungeon.actionManager.addToBottom(new AllEnemiesGainBlockAction(this.magicNumber));
+    }
+}
