8473f32e3296ce968b2800ecdcba60e475d812fb
18penning <8penning@informatik.uni-hamburg.de>
2022-03-30 18:00:37 +0200

    Added a new Card called SharedBurden.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/SharedBurden.java
@@ -0,0 +1,49 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.AllEnemiesWeakAction;
+import theUnchainedMod.characters.TheDefault;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class SharedBurden extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(SharedBurden.class.getSimpleName());
+    public static final String IMG = makeCardPath("SharedBurden.png");
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 0;
+    private static final int MAGIC_NUMBER = 2;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = -1;
+    private static final int SECOND_MAGIC_NUMBER = 1;
+    private static final int UPGRADE_PLUS_SECOND_MAGIC_NUMBER = 1;
+
+    public SharedBurden() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
+            upgradeDefaultSecondMagicNumber(UPGRADE_PLUS_SECOND_MAGIC_NUMBER);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new WeakPower(p, magicNumber, false)));
+        AbstractDungeon.actionManager.addToBottom(new AllEnemiesWeakAction(defaultSecondMagicNumber));
+    }
+}
