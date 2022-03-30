06dcd345ad69bce83a161c04f0918ce7997ad2eb
18penning <8penning@informatik.uni-hamburg.de>
2022-03-30 13:56:07 +0200

    Added a new Card called Reschedule and its Power and Action

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/RescheduleAction.java
@@ -0,0 +1,27 @@
+package theUnchainedMod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import theUnchainedMod.powers.NextTurnRelayedDamagePower;
+
+public class RescheduleAction extends AbstractGameAction {
+    private static final String relayedDamage = "theUnchainedMod:RelayedDamagePower";
+    private final AbstractPlayer player;
+
+    public RescheduleAction(AbstractPlayer p) {
+        player = p;
+    }
+
+    @Override
+    public void update() {
+        if(player.hasPower(relayedDamage)) {
+            int rescheduleAmount = player.getPower(relayedDamage).amount;
+            AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(player, player, relayedDamage));
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(player, player, new NextTurnRelayedDamagePower(player, player, rescheduleAmount)));
+        }
+        this.isDone = true;
+    }
+}
