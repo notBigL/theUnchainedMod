849abab646caaaf935359616c05239cdb6c024c6
18penning <8penning@informatik.uni-hamburg.de>
2022-03-31 21:02:22 +0200

    Finished the new HookedChain card.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/HookedChain.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/HookedChain.java
@@ -1,11 +1,13 @@
 package theUnchainedMod.cards;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theUnchainedMod.DefaultMod;
-import theUnchainedMod.actions.SliceThroughAction;
 import theUnchainedMod.characters.TheDefault;
 
 import static theUnchainedMod.DefaultMod.makeCardPath;
@@ -13,14 +15,14 @@ import static theUnchainedMod.DefaultMod.makeCardPath;
 public class HookedChain extends AbstractDynamicCard {
 
     public static final String ID = DefaultMod.makeID(HookedChain.class.getSimpleName());
-    public static final String IMG = makeCardPath("SliceThrough.png");
+    public static final String IMG = makeCardPath("HookedChain.png");
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
     private static final int COST = 2;
-    private static final int DAMAGE = 11;
+    private static final int DAMAGE = 8;
     private static final int UPGRADE_PLUS_DMG = 4;
 
     public HookedChain() {
@@ -39,6 +41,7 @@ public class HookedChain extends AbstractDynamicCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new SliceThroughAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), magicNumber, p));
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        AbstractDungeon.actionManager.addToBottom(new MakeTempCardInHandAction(new HiddenBlade(), 1, false));
     }
 }
