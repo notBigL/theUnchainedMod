dcc70d72530293a6c93e7ac09399b056cf3d1f02
18penning <8penning@informatik.uni-hamburg.de>
2022-04-09 14:18:31 +0200

    Added a new card called Burnish and its Action.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/BurnishAction.java
@@ -0,0 +1,56 @@
+package theUnchainedMod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import theUnchainedMod.powers.MomentumPower;
+
+import java.util.Iterator;
+
+public class BurnishAction extends AbstractGameAction {
+    private final AbstractPlayer player;
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("RecycleAction");
+    public static final String[] TEXT = uiStrings.TEXT;
+
+
+    public BurnishAction() {
+        this.actionType = ActionType.CARD_MANIPULATION;
+        this.player = AbstractDungeon.player;
+        this.duration = Settings.ACTION_DUR_FAST;
+    }
+
+    public void update() {
+        if (this.duration == Settings.ACTION_DUR_FAST) {
+            if (this.player.hand.isEmpty()) {
+                this.isDone = true;
+            } else if (this.player.hand.size() == 1) {
+                if (this.player.hand.getBottomCard().costForTurn > 0) {
+                    AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(player, player, new MomentumPower(player, this.player.hand.getBottomCard().costForTurn)));
+                }
+                this.player.hand.moveToExhaustPile(this.player.hand.getBottomCard());
+                this.tickDuration();
+            } else {
+                AbstractDungeon.handCardSelectScreen.open(TEXT[0], 1, false);
+                this.tickDuration();
+            }
+        } else {
+            if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
+                AbstractCard c;
+                for (Iterator var1 = AbstractDungeon.handCardSelectScreen.selectedCards.group.iterator(); var1.hasNext(); this.player.hand.moveToExhaustPile(c)) {
+                    c = (AbstractCard) var1.next();
+                    if (this.player.hand.getBottomCard().costForTurn > 0) {
+                        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(player, player, new MomentumPower(player, this.player.hand.getBottomCard().costForTurn)));
+                    }
+                }
+                AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+                AbstractDungeon.handCardSelectScreen.selectedCards.group.clear();
+            }
+            this.tickDuration();
+        }
+    }
+}
---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Burnish.java
@@ -0,0 +1,44 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.BurnishAction;
+import theUnchainedMod.characters.TheDefault;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class Burnish extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(Burnish.class.getSimpleName());
+    public static final String IMG = makeCardPath("Burnish.png");
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int MAGIC_NUMBER = 1;
+
+    public Burnish() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        this.cardsToPreview = new Swirl();
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeBaseCost(0);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new BurnishAction());
+        AbstractDungeon.actionManager.addToBottom(new MakeTempCardInHandAction(new Swirl(), magicNumber, false));
+    }
+}
