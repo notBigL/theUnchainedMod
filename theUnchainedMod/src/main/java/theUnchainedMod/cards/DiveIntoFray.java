14e61780cb369aa4252128c8144c7457d68d770d
18penning <8penning@informatik.uni-hamburg.de>
2022-04-19 00:23:08 +0200

    Added a new Card called DiveIntoFray.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/DiveIntoFray.java
@@ -0,0 +1,55 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.RelayedDamagePower;
+
+import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class DiveIntoFray extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(DiveIntoFray.class.getSimpleName());
+    public static final String IMG = makeCardPath("DiveIntoFray.png");
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+    public static final String UPGRADE_DESCRIPTION = languagePack.getCardStrings(ID).UPGRADE_DESCRIPTION;
+
+    private static final int COST = 0;
+    private static final int MAGIC_NUMBER = 6;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = -1;
+    private static final int SECOND_MAGIC_NUMBER = 2;
+    private static final int UPGRADE_PLUS_SECOND_MAGIC_NUMBER = 1;
+
+
+    public DiveIntoFray() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
+        this.exhaust = true;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
+            upgradeDefaultSecondMagicNumber(UPGRADE_PLUS_SECOND_MAGIC_NUMBER);
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new GainEnergyAction(defaultSecondMagicNumber));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new RelayedDamagePower(p, p, magicNumber)));
+    }
+}
