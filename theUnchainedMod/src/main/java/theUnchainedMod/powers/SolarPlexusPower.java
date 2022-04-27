d3d8d58c42b456bf2633890f60676b23d3a4409d
18penning <8penning@informatik.uni-hamburg.de>
2022-04-27 18:29:20 +0200

    SolarPlexus Overhaul. The Damage is now the immediate use and the Stun is te Chain.

--- theUnchainedMod/src/main/java/theUnchainedMod/powers/SolarPlexusPower.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/powers/SolarPlexusPower.java
@@ -1,14 +1,13 @@
 package theUnchainedMod.powers;
 
+import com.evacipated.cardcrawl.mod.stslib.actions.common.StunMonsterAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import theUnchainedMod.DefaultMod;
-import theUnchainedMod.actions.MultiAttackAction;
 
 public class SolarPlexusPower extends AbstractChainPower {
     public AbstractCreature source;
@@ -19,25 +18,19 @@ public class SolarPlexusPower extends AbstractChainPower {
     public static final String[] DESCRIPTIONS = powerstrings.DESCRIPTIONS;
     public static AbstractPower.PowerType POWER_TYPE = PowerType.BUFF;
 
-    private final AbstractMonster monster;
-    private final int damageAmount;
-    private final int attackAmount;
 
-    public SolarPlexusPower(AbstractCreature owner, int amount, AbstractMonster monster, int damageAmount, int attackAmount, AbstractCard.CardType cardType) {
-        super(POWER_ID, owner, amount, new MultiAttackAction(attackAmount, monster, new DamageInfo(owner, damageAmount, DamageInfo.DamageType.NORMAL)), cardType);
+    public SolarPlexusPower(AbstractCreature owner, int amount, AbstractMonster monster, AbstractCard.CardType cardType) {
+        super(POWER_ID, owner, amount, new StunMonsterAction(monster, owner), cardType);
         this.name = NAME;
         this.type = POWER_TYPE;
-        this.monster = monster;
-        this.damageAmount = damageAmount;
-        this.attackAmount = attackAmount;
         this.updateDescription();
     }
 
     public void updateDescription() {
         if (this.amount == 1) {
-            this.description = DESCRIPTIONS[0] + this.damageAmount + DESCRIPTIONS[1] + this.attackAmount + DESCRIPTIONS[2];
+            this.description = DESCRIPTIONS[0];
         } else {
-            this.description = DESCRIPTIONS[3] + this.amount + DESCRIPTIONS[4] + this.damageAmount + DESCRIPTIONS[1] + this.attackAmount + DESCRIPTIONS[2];
+            this.description = DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[2];
         }
     }
 }
