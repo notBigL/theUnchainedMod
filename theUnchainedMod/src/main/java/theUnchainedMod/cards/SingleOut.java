c5858a722b15b908d7532a050bd40a06d97b8557
18penning <8penning@informatik.uni-hamburg.de>
2022-04-28 01:20:37 +0200

    A little Buff for SingleOut.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/SingleOut.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/SingleOut.java
@@ -21,10 +21,10 @@ public class SingleOut extends AbstractDynamicCard {
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 11;
+    private static final int DAMAGE = 12;
     private static final int UPGRADE_PLUS_DMG = 3;
-    private static final int MAGIC_NUMBER = 6;
-    private static final int UPGRADE_PLUS_MAGIC_NUMBER = -2;
+    private static final int MAGIC_NUMBER = 3;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = -1;
 
     public SingleOut() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
