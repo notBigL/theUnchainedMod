194058504c0da03436012745c2d3665c44e6f567
18penning <8penning@informatik.uni-hamburg.de>
2022-04-28 16:56:27 +0200

    ShieldFace is now common.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/ShieldFace.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/ShieldFace.java
@@ -15,7 +15,7 @@ public class ShieldFace extends AbstractDynamicCard {
 
     public static final String ID = DefaultMod.makeID(ShieldFace.class.getSimpleName());
     public static final String IMG = makeCardPath("ShieldFace.png");
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
