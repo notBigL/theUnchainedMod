8bb5aebda769132c08d02c5e413f9fcc78509900
18penning <8penning@informatik.uni-hamburg.de>
2022-04-29 12:49:40 +0200

    Upgrading ThreeSixty is less strong now.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/ThreeSixty.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/ThreeSixty.java
@@ -24,7 +24,7 @@ public class ThreeSixty extends AbstractDynamicCard {
 
     private static final int COST = 1;
     private static final int DAMAGE = 5;
-    private static final int UPGRADE_PLUS_DMG = 3;
+    private static final int UPGRADE_PLUS_DMG = 2;
     private static final int MAGIC_NUMBER = 4;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
 
