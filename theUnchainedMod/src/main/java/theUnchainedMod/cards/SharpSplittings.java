dfbe793eff57e6c92f0a0c2feed990dbb9ee8a44
18penning <8penning@informatik.uni-hamburg.de>
2022-04-29 14:57:53 +0200

    Finished the new card called SharpSplittings.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/SharpSplittings.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/SharpSplittings.java
@@ -2,13 +2,14 @@ package theUnchainedMod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.FrailPower;
 import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.AllEnemiesVulnerableAction;
+import theUnchainedMod.actions.AllEnemiesWeakAction;
 import theUnchainedMod.characters.TheDefault;
 
 import static theUnchainedMod.DefaultMod.makeCardPath;
@@ -22,14 +23,17 @@ public class SharpSplittings extends AbstractDynamicCard {
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 13;
-    private static final int UPGRADE_PLUS_DAMAGE = 4;
-    private static final int MAGIC_NUMBER = 2;
+    private static final int DAMAGE = 5;
+    private static final int UPGRADE_PLUS_DAMAGE = 3;
+    private static final int MAGIC_NUMBER = 1;
+    private static final int SECOND_MAGIC_NUMBER = 2;
 
     public SharpSplittings() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
         baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
+        this.isMultiDamage = true;
     }
 
     @Override
@@ -43,7 +47,9 @@ public class SharpSplittings extends AbstractDynamicCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new FrailPower(p, this.magicNumber, false)));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new FrailPower(p, this.defaultBaseSecondMagicNumber, false)));
+        AbstractDungeon.actionManager.addToBottom(new AllEnemiesVulnerableAction(this.magicNumber));
+        AbstractDungeon.actionManager.addToBottom(new AllEnemiesWeakAction(this.magicNumber));
+        AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
     }
 }
