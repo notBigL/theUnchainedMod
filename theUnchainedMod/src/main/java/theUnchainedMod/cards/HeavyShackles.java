7e18a4e225d727a01e7d093439dba6c38c636847
18penning <8penning@informatik.uni-hamburg.de>
2022-05-02 01:07:12 +0200

    HeavyShackles now makes you draw an additional card aswell.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/HeavyShackles.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/HeavyShackles.java
@@ -4,6 +4,7 @@ import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DrawPower;
 import com.megacrit.cardcrawl.powers.watcher.EnergyDownPower;
 import theUnchainedMod.DefaultMod;
 import theUnchainedMod.characters.TheDefault;
@@ -24,10 +25,12 @@ public class HeavyShackles extends AbstractDynamicCard {
 
     private static final int COST = 1;
     private static final int MAGIC_NUMBER = 2;
+    private static final int SECOND_MAGIC_NUMBER = 2;
 
     public HeavyShackles() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
     }
 
     public void upgrade() {
@@ -43,5 +46,6 @@ public class HeavyShackles extends AbstractDynamicCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new EnergyDownPower(p, this.magicNumber)));
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new HeavyShacklesPower(p, this.upgraded)));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new DrawPower(p, defaultSecondMagicNumber)));
     }
 }
