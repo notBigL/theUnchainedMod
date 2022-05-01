33b7314ca1577d83a02fb3789a14e07e0510ce34
18penning <8penning@informatik.uni-hamburg.de>
2022-05-02 00:11:02 +0200

    BaguazhangAction now works correctly.

--- theUnchainedMod/src/main/java/theUnchainedMod/actions/BaguazhangAction.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/BaguazhangAction.java
@@ -20,7 +20,13 @@ public class BaguazhangAction extends AbstractGameAction {
 
     @Override
     public void update() {
-        if (targetMonster != null && targetMonster.getIntentDmg() < damageAmount) {
+        if (targetMonster != null && (targetMonster.getIntentDmg() < damageAmount || targetMonster.intent == AbstractMonster.Intent.BUFF ||
+                targetMonster.intent == AbstractMonster.Intent.DEBUFF || targetMonster.intent == AbstractMonster.Intent.STRONG_DEBUFF ||
+                targetMonster.intent == AbstractMonster.Intent.DEBUG || targetMonster.intent == AbstractMonster.Intent.DEFEND ||
+                targetMonster.intent == AbstractMonster.Intent.DEFEND_DEBUFF || targetMonster.intent == AbstractMonster.Intent.DEFEND_BUFF ||
+                targetMonster.intent == AbstractMonster.Intent.ESCAPE || targetMonster.intent == AbstractMonster.Intent.MAGIC ||
+                targetMonster.intent == AbstractMonster.Intent.NONE || targetMonster.intent == AbstractMonster.Intent.SLEEP ||
+                targetMonster.intent == AbstractMonster.Intent.STUN || targetMonster.intent == AbstractMonster.Intent.UNKNOWN)) {
             AbstractDungeon.actionManager.addToBottom(new GainBlockAction(source, amount));
         }
         this.isDone = true;
