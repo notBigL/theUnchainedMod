194078684bce39f68853c31ddbd258934f9dd085
18penning <8penning@informatik.uni-hamburg.de>
2022-05-03 23:28:56 +0200

    Added a new card called GlimpseIntoFuture and its power.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/GlimpseIntoFuture.java
@@ -0,0 +1,49 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.GlimpseIntoFuturePower;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class GlimpseIntoFuture extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(GlimpseIntoFuture.class.getSimpleName());
+    public static final String IMG = makeCardPath("GlimpseIntoFuture.png");
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 6;
+    private static final int UPGRADE_PLUS_BLOCK = 2;
+    private static final int MAGIC_NUMBER = 2;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
+
+    public GlimpseIntoFuture() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = block = BLOCK;
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, block));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new GlimpseIntoFuturePower(p, magicNumber)));
+    }
+}
---
+++ theUnchainedMod/src/main/java/theUnchainedMod/powers/GlimpseIntoFuturePower.java
@@ -0,0 +1,68 @@
+package theUnchainedMod.powers;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.util.TextureLoader;
+
+public class GlimpseIntoFuturePower extends AbstractPower {
+
+    public static final String POWER_ID = DefaultMod.makeID("GlimpseIntoFuturePower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    private static final Texture texture48 = TextureLoader.getTexture("theUnchainedModResources/images/powers/GlimpseIntoFuturePower_power48.png");
+    private static final Texture texture128 = TextureLoader.getTexture("theUnchainedModResources/images/powers/GlimpseIntoFuturePower_power128.png");
+
+    private boolean energyDepleted;
+
+    public GlimpseIntoFuturePower(final AbstractCreature owner, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        type = AbstractPower.PowerType.BUFF;
+        isTurnBased = false;
+        energyDepleted = false;
+
+        this.region128 = new TextureAtlas.AtlasRegion(texture128, 0, 0, 128, 128);
+        this.region48 = new TextureAtlas.AtlasRegion(texture48, 0, 0, 48, 48);
+
+        updateDescription();
+    }
+
+
+    public void updateDescription() {
+        if (energyDepleted) {
+            this.description = DESCRIPTIONS[2] + this.amount + DESCRIPTIONS[3];
+        } else {
+            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+        }
+    }
+
+    public void atEndOfTurn(boolean isPlayer) {
+        if (isPlayer) {
+            if (EnergyPanel.getCurrentEnergy() == 0) {
+                energyDepleted = true;
+                updateDescription();
+            }
+        }
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        if (energyDepleted) {
+            AbstractDungeon.actionManager.addToBottom(new GainEnergyAction(this.amount));
+        }
+        AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(owner, owner, this));
+    }
+}
