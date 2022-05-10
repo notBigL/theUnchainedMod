0ed1f2b042d4ef61beb3c99af44350d3b6e4b078
18penning <8penning@informatik.uni-hamburg.de>
2022-05-11 00:01:51 +0200

    Added a new card called BlindSwing and its power.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/BlindSwing.java
@@ -0,0 +1,50 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.MultiAttackAction;
+import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.BlindSwingPower;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class BlindSwing extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(BlindSwing.class.getSimpleName());
+    public static final String IMG = makeCardPath("BlindSwing.png");
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 0;
+    private static final int DAMAGE = 4;
+    private static final int UPGRADE_PLUS_DMG = 2;
+    private static final int MAGIC_NUMBER = 2;
+    private static final int SECOND_MAGIC_NUMBER = 2;
+
+    public BlindSwing() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new MultiAttackAction(2, m, new DamageInfo(p, this.damage, this.damageTypeForTurn)));
+        AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(p, p, new BlindSwingPower(p, defaultSecondMagicNumber, magicNumber, TYPE)));
+    }
+}
