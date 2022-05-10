b9448c366499eefec5565feb5ccce82a2dad823a
18penning <8penning@informatik.uni-hamburg.de>
2022-05-10 21:20:42 +0200

    Added a new card called EscapeRope.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/BasicDefend.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/BasicDefend.java
@@ -1,10 +1,6 @@
 package theUnchainedMod.cards;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.common.UpgradeRandomCardAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
