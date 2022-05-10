8f58f3af81af618f88872cb392b6287177a4ff74
18penning <8penning@informatik.uni-hamburg.de>
2022-05-10 22:04:29 +0200

    Added a new card called LawOfInertia and its Powers.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/LawOfInertia.java
@@ -0,0 +1,46 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.LawOfInertiaPower;
+
+import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class LawOfInertia extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(LawOfInertia.class.getSimpleName());
+    public static final String IMG = makeCardPath("LawOfInertia.png");
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+    public static final String UPGRADE_DESCRIPTION = languagePack.getCardStrings(ID).UPGRADE_DESCRIPTION;
+
+    private static final int COST = 0;
+    private static final int MAGIC_NUMBER = 1;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
+
+
+    public LawOfInertia() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new LawOfInertiaPower(p, p, magicNumber)));
+    }
+}
