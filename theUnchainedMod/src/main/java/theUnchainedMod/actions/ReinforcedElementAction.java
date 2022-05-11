f2ed0270acf6747453c69c946c4a843d3a4edf05
18penning <8penning@informatik.uni-hamburg.de>
2022-05-11 14:13:51 +0200

    Added a new card called ReinforcedElement and its Action.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/ReinforcedElementAction.java
@@ -0,0 +1,27 @@
+package theUnchainedMod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import theUnchainedMod.powers.AbstractChainPower;
+
+public class ReinforcedElementAction extends AbstractGameAction {
+
+
+    @Override
+    public void update() {
+        AbstractPlayer player = AbstractDungeon.player;
+
+        for (AbstractPower p : player.powers) {
+            if (p instanceof AbstractChainPower) {
+                this.addToTop(new DrawCardAction(player, 1));
+                this.addToTop(new GainEnergyAction(1));
+                break;
+            }
+        }
+        this.isDone = true;
+    }
+}
