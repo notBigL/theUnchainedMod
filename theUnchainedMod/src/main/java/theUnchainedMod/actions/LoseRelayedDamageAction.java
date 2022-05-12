f32ba452e0755ae430afe213aa66643cd986ed25
18penning <8penning@informatik.uni-hamburg.de>
2022-05-12 21:03:27 +0200

    FigureEightKnot now loses relayedDamage instead of giving Knot.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/LoseRelayedDamageAction.java
@@ -0,0 +1,29 @@
+package theUnchainedMod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class LoseRelayedDamageAction extends AbstractGameAction {
+
+    public LoseRelayedDamageAction(int relayedDamageToLose) {
+        this.amount = relayedDamageToLose;
+    }
+
+
+    @Override
+    public void update() {
+        AbstractPlayer player = AbstractDungeon.player;
+        if (player.hasPower("theUnchainedMod:RelayedDamagePower")) {
+            AbstractPower relayedDamage = player.getPower("theUnchainedMod:RelayedDamagePower");
+            if (this.amount < relayedDamage.amount) {
+                relayedDamage.amount -= this.amount;
+            } else {
+                AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(player, player, relayedDamage));
+            }
+            this.isDone = true;
+        }
+    }
+}
--- theUnchainedMod/src/main/java/theUnchainedMod/cards/FigureEightKnot.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/FigureEightKnot.java
@@ -5,6 +5,7 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.LoseRelayedDamageAction;
 import theUnchainedMod.characters.TheDefault;
 import theUnchainedMod.powers.KnotPower;
 import theUnchainedMod.powers.RelayPower;
@@ -20,19 +21,15 @@ public class FigureEightKnot extends AbstractDynamicCard {
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
-    public static final String UPGRADE_DESCRIPTION = languagePack.getCardStrings(ID).UPGRADE_DESCRIPTION;
 
 
     private static final int COST = 2;
-    private static final int MAGIC_NUMBER = 1;
-    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
-    private static final int SECOND_MAGIC_NUMBER = 11;
-    private static final int UPGRADE_PLUS_SECOND_MAGIC_NUMBER = 3;
+    private static final int MAGIC_NUMBER = 9;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 3;
 
     public FigureEightKnot() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseMagicNumber = magicNumber = MAGIC_NUMBER;
-        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
     }
 
     @Override
@@ -40,15 +37,12 @@ public class FigureEightKnot extends AbstractDynamicCard {
         if (!upgraded) {
             upgradeName();
             upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
-            upgradeDefaultSecondMagicNumber(UPGRADE_PLUS_SECOND_MAGIC_NUMBER);
-            this.rawDescription = UPGRADE_DESCRIPTION;
-            this.initializeDescription();
         }
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new RelayPower(p, p, defaultSecondMagicNumber)));
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new KnotPower(p, magicNumber)));
+        AbstractDungeon.actionManager.addToBottom(new LoseRelayedDamageAction(magicNumber));
     }
 }
