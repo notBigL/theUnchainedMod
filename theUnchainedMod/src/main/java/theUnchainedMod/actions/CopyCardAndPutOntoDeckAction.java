9537e5fc0f20ddf310880c4e2663f3a908632048
18penning <8penning@informatik.uni-hamburg.de>
2022-05-12 23:08:09 +0200

    Added a new card called GraspIntoFuture and its action.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/CopyCardAndPutOntoDeckAction.java
@@ -0,0 +1,56 @@
+package theUnchainedMod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+
+public class CopyCardAndPutOntoDeckAction extends AbstractGameAction {
+
+    private final AbstractPlayer p;
+    private final boolean upgraded;
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("CopyAction");
+    public static final String[] TEXT = uiStrings.TEXT;
+
+    public CopyCardAndPutOntoDeckAction(boolean upgraded) {
+        this.actionType = ActionType.CARD_MANIPULATION;
+        this.p = AbstractDungeon.player;
+        this.upgraded = upgraded;
+        this.duration = Settings.ACTION_DUR_FAST;
+    }
+
+    @Override
+    public void update() {
+        AbstractCard c;
+        if (this.duration == Settings.ACTION_DUR_FAST) {
+            if (p.hand.size() == 0) {
+                this.isDone = true;
+            } else if (p.hand.size() == 1) {
+                c = this.p.hand.getTopCard();
+                this.p.hand.moveToDeck(c.makeStatEquivalentCopy(), false);
+                if (upgraded) {
+                    this.p.hand.moveToDeck(c.makeStatEquivalentCopy(), false);
+                }
+                this.tickDuration();
+            } else {
+                AbstractDungeon.handCardSelectScreen.open(TEXT[0], 1, false);
+                this.tickDuration();
+            }
+        } else {
+            if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
+                c = AbstractDungeon.handCardSelectScreen.selectedCards.getTopCard();
+                this.p.hand.moveToDeck(c.makeStatEquivalentCopy(), false);
+                if (upgraded) {
+                    this.p.hand.moveToDeck(c.makeStatEquivalentCopy(), false);
+                }
+                AbstractDungeon.player.hand.addToHand(c);
+                AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+                AbstractDungeon.handCardSelectScreen.selectedCards.group.clear();
+            }
+        }
+        this.tickDuration();
+    }
+}
---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/GraspIntoFuture.java
@@ -0,0 +1,44 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.CopyCardAndPutOntoDeckAction;
+import theUnchainedMod.characters.TheDefault;
+
+import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class GraspIntoFuture extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(GraspIntoFuture.class.getSimpleName());
+    public static final String IMG = makeCardPath("WeatherbeatenSkin.png");
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+    public static final String UPGRADE_DESCRIPTION = languagePack.getCardStrings(ID).UPGRADE_DESCRIPTION;
+
+
+    private static final int COST = 2;
+
+    public GraspIntoFuture() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.exhaust = true;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new CopyCardAndPutOntoDeckAction(this.upgraded));
+    }
+}
