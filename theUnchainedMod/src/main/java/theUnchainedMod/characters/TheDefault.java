a9cfc83154ab4e291f5acbcbce20322cca49c757
18penning <8penning@informatik.uni-hamburg.de>
2022-05-18 23:40:57 +0200

    TheUnchained now has a real Description and now longer has OrbSlots.

--- theUnchainedMod/src/main/java/theUnchainedMod/characters/TheDefault.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/characters/TheDefault.java
@@ -62,11 +62,11 @@ public class TheDefault extends CustomPlayer {
     // =============== BASE STATS =================
 
     public static final int ENERGY_PER_TURN = 3;
-    public static final int STARTING_HP = 75;
-    public static final int MAX_HP = 75;
+    public static final int STARTING_HP = 78;
+    public static final int MAX_HP = 78;
     public static final int STARTING_GOLD = 99;
     public static final int CARD_DRAW = 5;
-    public static final int ORB_SLOTS = 3;
+    public static final int ORB_SLOTS = 0;
 
     // =============== /BASE STATS/ =================
 
