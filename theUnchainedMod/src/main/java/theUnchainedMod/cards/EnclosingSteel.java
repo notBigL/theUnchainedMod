9fc34a5ebd5132529177f73b13bb5a284569f0bc
18penning <8penning@informatik.uni-hamburg.de>
2022-05-18 22:18:49 +0200

    StepBack gives 1 more Block. EnclosingSteel is now common.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/EnclosingSteel.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/EnclosingSteel.java
@@ -15,7 +15,7 @@ public class EnclosingSteel extends AbstractDynamicCard {
 
     public static final String ID = DefaultMod.makeID(EnclosingSteel.class.getSimpleName());
     public static final String IMG = makeCardPath("EnclosingSteel.png");
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
--- theUnchainedMod/src/main/java/theUnchainedMod/cards/StepBack.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/StepBack.java
@@ -23,7 +23,7 @@ public class StepBack extends AbstractDynamicCard {
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
     private static final int COST = 1;
-    private static final int BLOCK = 5;
+    private static final int BLOCK = 6;
     private static final int UPGRADE_PLUS_BLOCK = 4;
     private static final int MAGIC_NUMBER = 2;
     private static final int SECOND_MAGIC_NUMBER = 1;
