756f70ebc0ac32fe76b6cbd3c15f343b71063931
18penning <8penning@informatik.uni-hamburg.de>
2022-06-09 18:16:51 +0200

    Instincts now costs 2.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/Instincts.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Instincts.java
@@ -20,7 +20,7 @@ public class Instincts extends AbstractDynamicCard {
     private static final CardType TYPE = CardType.POWER;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
-    private static final int COST = 1;
+    private static final int COST = 2;
     private static final int MAGIC_NUMBER = 1;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
 
