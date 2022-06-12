a413adc23358398de528a876f6b38d1fab581d44
18penning <8penning@informatik.uni-hamburg.de>
2022-06-12 22:00:35 +0200

    LongGame now works with a new Action and synergies with Frail.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/LongGameAction.java
@@ -0,0 +1,26 @@
+package theUnchainedMod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
+import theUnchainedMod.powers.RelayPower;
+
+public class LongGameAction extends AbstractGameAction {
+
+    public LongGameAction (AbstractPlayer player, int relayAmount) {
+        this.amount = relayAmount;
+        this.target = player;
+    }
+
+
+    @Override
+    public void update() {
+        if(target.hasPower("Frail")) {
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(target, target, new RelayPower(target, target, amount)));
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(target, target, new DrawCardNextTurnPower(target, 1), 1));
+        }
+        this.isDone = true;
+    }
+}
--- theUnchainedMod/src/main/java/theUnchainedMod/cards/LongGame.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/LongGame.java
@@ -7,6 +7,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.FrailPower;
 import theUnchainedMod.DefaultMod;
 import theUnchainedMod.actions.AllEnemiesVulnerableAction;
+import theUnchainedMod.actions.LongGameAction;
 import theUnchainedMod.characters.TheDefault;
 import theUnchainedMod.powers.LongGamePower;
 import theUnchainedMod.powers.RelayPower;
@@ -23,24 +24,18 @@ public class LongGame extends AbstractDynamicCard {
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
     private static final int COST = 1;
-    private static final int BLOCK = 10;
-    private static final int UPGRADE_PLUS_BLOCK = 4;
     private static final int MAGIC_NUMBER = 9;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 3;
-    private static final int SECOND_MAGIC_NUMBER = 1;
 
     public LongGame() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        baseBlock = block = BLOCK;
         baseMagicNumber = magicNumber = MAGIC_NUMBER;
-        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
     }
 
     @Override
     public void upgrade() {
         if(!upgraded) {
             upgradeName();
-            upgradeBlock(UPGRADE_PLUS_BLOCK);
             upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
         }
     }
@@ -48,6 +43,6 @@ public class LongGame extends AbstractDynamicCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new RelayPower(p, p, magicNumber)));
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new LongGamePower(p, block, defaultSecondMagicNumber)));
+        AbstractDungeon.actionManager.addToBottom(new LongGameAction(p, magicNumber));
     }
 }
