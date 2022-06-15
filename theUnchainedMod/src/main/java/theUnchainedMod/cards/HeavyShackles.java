2cf973a7d0035ae42523c057c227ecab4840fb89
18penning <8penning@informatik.uni-hamburg.de>
2022-06-15 15:20:35 +0200

    HeavyShacklesOverhaul.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/HeavyShackles.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/HeavyShackles.java
@@ -7,6 +7,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.DrawPower;
 import com.megacrit.cardcrawl.powers.watcher.EnergyDownPower;
 import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.MakeTwoCostCardsFreeAction;
 import theUnchainedMod.characters.TheDefault;
 import theUnchainedMod.powers.HeavyShacklesPower;
 
@@ -23,14 +24,10 @@ public class HeavyShackles extends AbstractDynamicCard {
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
     public static final String UPGRADE_DESCRIPTION = languagePack.getCardStrings(ID).UPGRADE_DESCRIPTION;
 
-    private static final int COST = 1;
-    private static final int MAGIC_NUMBER = 2;
-    private static final int SECOND_MAGIC_NUMBER = 1;
+    private static final int COST = 3;
 
     public HeavyShackles() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        baseMagicNumber = magicNumber = MAGIC_NUMBER;
-        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
     }
 
     public void upgrade() {
@@ -44,8 +41,7 @@ public class HeavyShackles extends AbstractDynamicCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new EnergyDownPower(p, this.magicNumber)));
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new HeavyShacklesPower(p, this.upgraded)));
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new DrawPower(p, defaultSecondMagicNumber)));
+        AbstractDungeon.actionManager.addToBottom(new MakeTwoCostCardsFreeAction(this.upgraded));
     }
 }
