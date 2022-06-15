d88401af8bc91aed5bf54c289c0997fcd3b62ae1
18penning <8penning@informatik.uni-hamburg.de>
2022-06-15 16:08:26 +0200

    SpinningWheel no longer has a Chain.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/SpinningWheel.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/SpinningWheel.java
@@ -10,6 +10,7 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theUnchainedMod.DefaultMod;
 import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.MomentumPower;
 import theUnchainedMod.powers.SpinningWheelPower;
 
 import static theUnchainedMod.DefaultMod.makeCardPath;
@@ -18,7 +19,7 @@ public class SpinningWheel extends AbstractDynamicCard {
 
     public static final String ID = DefaultMod.makeID(SpinningWheel.class.getSimpleName());
     public static final String IMG = makeCardPath("Swathe.png");
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
@@ -30,14 +31,12 @@ public class SpinningWheel extends AbstractDynamicCard {
     private static final int UPGRADE_PLUS_BLOCK = 1;
     private static final int MAGIC_NUMBER = 1;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
-    private static final int SECOND_MAGIC_NUMBER = 1;
 
     public SpinningWheel() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
         baseBlock = block = BLOCK;
         baseMagicNumber = magicNumber = MAGIC_NUMBER;
-        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
         this.cardsToPreview = new Swirl();
     }
 
@@ -56,6 +55,6 @@ public class SpinningWheel extends AbstractDynamicCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
         AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, block));
-        AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(p, p, new SpinningWheelPower(p, defaultSecondMagicNumber, magicNumber, TYPE)));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new MomentumPower(p, magicNumber)));
     }
 }
