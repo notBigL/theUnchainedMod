67d01825bb98c5eeaba7209da508dfb1e0613a54
18penning <8penning@informatik.uni-hamburg.de>
2022-06-21 16:01:21 +0200

    ReinforcedElement now glows yellow when the Player has an AbstractChainPower.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/ReinforcedElement.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/ReinforcedElement.java
@@ -1,12 +1,17 @@
 package theUnchainedMod.cards;
 
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 import theUnchainedMod.DefaultMod;
 import theUnchainedMod.actions.ReinforcedElementAction;
 import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.AbstractChainPower;
+
+import java.util.Iterator;
 
 import static theUnchainedMod.DefaultMod.makeCardPath;
 
@@ -33,6 +38,19 @@ public class ReinforcedElement extends AbstractDynamicCard {
         if (!upgraded) {
             upgradeName();
             upgradeBlock(UPGRADE_PLUS_BLOCK);
+        }
+    }
+
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        Iterator var1 = AbstractDungeon.player.powers.iterator();
+
+        while(var1.hasNext()) {
+            AbstractPower power = (AbstractPower) var1.next();
+            if (power instanceof AbstractChainPower) {
+                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+                break;
+            }
         }
     }
 
