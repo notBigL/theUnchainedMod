fe2de6c938e52d140f0cbc325a4a8315c99747c8
18penning <8penning@informatik.uni-hamburg.de>
2022-06-21 15:53:16 +0200

    Baguazhang now glows yellow when it would work on an enemy.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/Baguazhang.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Baguazhang.java
@@ -1,11 +1,14 @@
 package theUnchainedMod.cards;
 
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theUnchainedMod.DefaultMod;
 import theUnchainedMod.actions.BaguazhangAction;
 import theUnchainedMod.characters.TheDefault;
+
+import java.util.Iterator;
 
 import static theUnchainedMod.DefaultMod.makeCardPath;
 
@@ -19,10 +22,10 @@ public class Baguazhang extends AbstractDynamicCard {
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
     private static final int COST = 1;
-    private static final int BLOCK = 18;
+    private static final int BLOCK = 14;
     private static final int UPGRADE_PLUS_BLOCK = 5;
-    private static final int MAGIC_NUMBER = 5;
-    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 2;
+    private static final int MAGIC_NUMBER = 7;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 3;
 
     public Baguazhang() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
@@ -37,6 +40,27 @@ public class Baguazhang extends AbstractDynamicCard {
             upgradeBlock(UPGRADE_PLUS_BLOCK);
             upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
         }
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        Iterator var1 = AbstractDungeon.getCurrRoom().monsters.monsters.iterator();
+
+        while(var1.hasNext()) {
+            AbstractMonster m = (AbstractMonster)var1.next();
+            if (m != null && (m.getIntentDmg() < magicNumber || m.intent == AbstractMonster.Intent.BUFF ||
+                    m.intent == AbstractMonster.Intent.DEBUFF || m.intent == AbstractMonster.Intent.STRONG_DEBUFF ||
+                    m.intent == AbstractMonster.Intent.DEBUG || m.intent == AbstractMonster.Intent.DEFEND ||
+                    m.intent == AbstractMonster.Intent.DEFEND_DEBUFF || m.intent == AbstractMonster.Intent.DEFEND_BUFF ||
+                    m.intent == AbstractMonster.Intent.ESCAPE || m.intent == AbstractMonster.Intent.MAGIC ||
+                    m.intent == AbstractMonster.Intent.NONE || m.intent == AbstractMonster.Intent.SLEEP ||
+                    m.intent == AbstractMonster.Intent.STUN || m.intent == AbstractMonster.Intent.UNKNOWN)) {
+                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+                break;
+            }
+        }
+
     }
 
     @Override
