9c322aefffaf97ae568ee898bcf80e403ec9accf
18penning <8penning@informatik.uni-hamburg.de>
2022-07-05 16:42:57 +0200

    Compounding now makes a card cheaper instead of upgrading it.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/CompoundAction.java
@@ -0,0 +1,116 @@
+package theUnchainedMod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class CompoundAction extends AbstractGameAction {
+
+    private static final UIStrings uiStrings;
+    public static final String[] TEXT;
+    private AbstractPlayer p;
+    private ArrayList<AbstractCard> cannotBeMadeCheaper = new ArrayList();
+
+    public CompoundAction() {
+        this.actionType = ActionType.CARD_MANIPULATION;
+        this.p = AbstractDungeon.player;
+        this.duration = Settings.ACTION_DUR_FAST;
+
+    }
+
+    @Override
+    public void update() {
+        Iterator var1;
+        AbstractCard c;
+        if (this.duration == Settings.ACTION_DUR_FAST) {
+
+            var1 = this.p.hand.group.iterator();
+
+            while(var1.hasNext()) {
+                c = (AbstractCard)var1.next();
+                if (c.costForTurn == 0) {
+                    this.cannotBeMadeCheaper.add(c);
+                }
+            }
+
+            if (this.cannotBeMadeCheaper.size() == this.p.hand.group.size()) {
+                this.isDone = true;
+                return;
+            }
+
+            if (this.p.hand.group.size() - this.cannotBeMadeCheaper.size() == 1) {
+                var1 = this.p.hand.group.iterator();
+
+                while(var1.hasNext()) {
+                    c = (AbstractCard)var1.next();
+                    if (c.costForTurn > 0) {
+                        int oldCost = c.costForTurn;
+                        c.setCostForTurn(oldCost - 1);
+                        c.superFlash();
+                        c.applyPowers();
+                        this.isDone = true;
+                        return;
+                    }
+                }
+            }
+
+            this.p.hand.group.removeAll(this.cannotBeMadeCheaper);
+            if (this.p.hand.group.size() > 1) {
+                AbstractDungeon.handCardSelectScreen.open(TEXT[0], 1, false, false, false, false);
+                this.tickDuration();
+                return;
+            }
+
+            if (this.p.hand.group.size() == 1) {
+                int oldCost = this.p.hand.getTopCard().costForTurn;
+                this.p.hand.getTopCard().setCostForTurn(oldCost - 1);
+                this.p.hand.getTopCard().superFlash();
+                this.returnCards();
+                this.isDone = true;
+            }
+        }
+
+        if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
+            var1 = AbstractDungeon.handCardSelectScreen.selectedCards.group.iterator();
+
+            while(var1.hasNext()) {
+                c = (AbstractCard)var1.next();
+                int oldCost = c.costForTurn;
+                c.setCostForTurn(oldCost - 1);
+                c.superFlash();
+                c.applyPowers();
+                this.p.hand.addToTop(c);
+            }
+
+            this.returnCards();
+            AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+            AbstractDungeon.handCardSelectScreen.selectedCards.group.clear();
+            this.isDone = true;
+        }
+
+        this.tickDuration();
+    }
+
+    private void returnCards() {
+        Iterator var1 = this.cannotBeMadeCheaper.iterator();
+
+        while(var1.hasNext()) {
+            AbstractCard c = (AbstractCard)var1.next();
+            this.p.hand.addToTop(c);
+        }
+
+        this.p.hand.refreshHandLayout();
+    }
+
+    static {
+        uiStrings = CardCrawlGame.languagePack.getUIString("ArmamentsAction");
+        TEXT = uiStrings.TEXT;
+    }
+}
--- theUnchainedMod/src/main/java/theUnchainedMod/cards/Compounding.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Compounding.java
@@ -6,6 +6,7 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.CompoundAction;
 import theUnchainedMod.characters.TheDefault;
 
 import static theUnchainedMod.DefaultMod.makeCardPath;
@@ -41,6 +42,6 @@ public class Compounding extends AbstractDynamicCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, block));
-        AbstractDungeon.actionManager.addToBottom(new ArmamentsAction(false));
+        AbstractDungeon.actionManager.addToBottom(new CompoundAction());
     }
 }
