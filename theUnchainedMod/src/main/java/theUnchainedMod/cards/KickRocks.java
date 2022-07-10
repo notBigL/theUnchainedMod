cf91cafdabab8272394e8e24b21e98c9ce83d7d7
18penning <8penning@informatik.uni-hamburg.de>
2022-07-10 18:04:03 +0200

    Added a new Card called KickRocks that replaces NonDominantHand.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/KickRocks.java
@@ -0,0 +1,53 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.DentedArmorPower;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class KickRocks extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(KickRocks.class.getSimpleName());
+    public static final String IMG = makeCardPath("KickRocks.png");
+    private static final AbstractCard.CardRarity RARITY = AbstractCard.CardRarity.COMMON;
+    private static final AbstractCard.CardTarget TARGET = AbstractCard.CardTarget.ENEMY;
+    private static final AbstractCard.CardType TYPE = AbstractCard.CardType.SKILL;
+    public static final AbstractCard.CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int MAGIC_NUMBER = 4;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 2;
+    private static final int SECOND_MAGIC_NUMBER = 1;
+    private static final int UPGRADE_PLUS_SECOND_MAGIC_NUMBER = 1;
+
+
+
+    public KickRocks() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
+        this.exhaust = true;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
+            upgradeDefaultSecondMagicNumber(UPGRADE_PLUS_SECOND_MAGIC_NUMBER);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new DentedArmorPower(m, p, magicNumber)));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new WeakPower(m, defaultSecondMagicNumber, false)));
+    }
+}
