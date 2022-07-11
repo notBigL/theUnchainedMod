fb9bbb8f88e445d9a704c7e2953935e83c02c271
18penning <8penning@informatik.uni-hamburg.de>
2022-07-11 20:16:47 +0200

    This Card should no longer haunt the game in weird ways.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/DefaultCommonAttack.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/DefaultCommonAttack.java
@@ -54,7 +54,7 @@ public class DefaultCommonAttack extends CustomCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
