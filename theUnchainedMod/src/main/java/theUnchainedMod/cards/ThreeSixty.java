eb5de80b8aa17611991f0b82d921c875dbe5870d
18penning <8penning@informatik.uni-hamburg.de>
2022-07-27 12:52:16 +0200

    ThreeSixty now deals damage two times instead of damage and HP loss.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/ThreeSixty.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/ThreeSixty.java
@@ -25,17 +25,13 @@ public class ThreeSixty extends AbstractDynamicCard {
 
     private static final int COST = 1;
     private static final int DAMAGE = 5;
-    private static final int UPGRADE_PLUS_DMG = 2;
-    private static final int MAGIC_NUMBER = 4;
+    private static final int MAGIC_NUMBER = 1;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
-    private static final int SECOND_MAGIC_NUMBER = 1;
-    private static final int UPGRADE_PLUS_SECOND_MAGIC_NUMBER = 1;
 
     public ThreeSixty() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
         baseMagicNumber = magicNumber = MAGIC_NUMBER;
-        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
         tags.add(CustomTags.SWING);
         this.cardsToPreview = new Swirl();
         this.isMultiDamage = true;
@@ -45,17 +41,15 @@ public class ThreeSixty extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeDamage(UPGRADE_PLUS_DMG);
             upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
-            upgradeDefaultSecondMagicNumber(UPGRADE_PLUS_SECOND_MAGIC_NUMBER);
         }
 
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.magicNumber, DamageInfo.DamageType.HP_LOSS), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
         AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new MomentumPower(p, defaultSecondMagicNumber)));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new MomentumPower(p, magicNumber)));
     }
 }
