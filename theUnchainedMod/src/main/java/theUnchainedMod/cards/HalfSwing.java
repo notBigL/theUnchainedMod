7cdd28d7decaeb2054357b0273a87d8413e1854c
18penning <8penning@informatik.uni-hamburg.de>
2022-07-27 13:21:37 +0200

    HalfSwing no scries instead of giving momentum.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/HalfSwing.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/HalfSwing.java
@@ -4,6 +4,7 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.utility.ScryAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -20,26 +21,22 @@ public class HalfSwing extends AbstractDynamicCard {
     public static final String ID = DefaultMod.makeID(HalfSwing.class.getSimpleName());
     public static final String IMG = makeCardPath("HalfSwing.png");
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
 
     private static final int COST = 1;
-    private static final int DAMAGE = 7;
-    private static final int UPGRADE_PLUS_DMG = 2;
-    private static final int MAGIC_NUMBER = 4;
-    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 2;
-    private static final int SECOND_MAGIC_NUMBER = 1;
-    private static final int UPGRADE_PLUS_SECOND_MAGIC_NUMBER = 1;
+    private static final int DAMAGE = 11;
+    private static final int UPGRADE_PLUS_DMG = 3;
+    private static final int MAGIC_NUMBER = 3;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
 
     public HalfSwing() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
         baseMagicNumber = magicNumber = MAGIC_NUMBER;
-        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
         tags.add(CustomTags.SWING);
-        this.cardsToPreview = new Swirl();
     }
 
     @Override
@@ -48,16 +45,13 @@ public class HalfSwing extends AbstractDynamicCard {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DMG);
             upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
-            upgradeDefaultSecondMagicNumber(UPGRADE_PLUS_SECOND_MAGIC_NUMBER);
         }
 
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, magicNumber, DamageInfo.DamageType.HP_LOSS), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
         AbstractDungeon.actionManager.addToBottom(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new MomentumPower(p, defaultSecondMagicNumber)));
-
+        AbstractDungeon.actionManager.addToBottom(new ScryAction(magicNumber));
     }
 }
