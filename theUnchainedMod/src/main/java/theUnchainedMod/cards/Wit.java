31d4cd047f52fc7c0a93959ec1e19c609c530dad
18penning <8penning@informatik.uni-hamburg.de>
2022-07-28 02:04:10 +0200

    Wit now has the momentum tag and updates its Description when upgraded

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/Wit.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Wit.java
@@ -8,6 +8,7 @@ import theUnchainedMod.DefaultMod;
 import theUnchainedMod.actions.WitAction;
 import theUnchainedMod.characters.TheDefault;
 
+import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
 import static theUnchainedMod.DefaultMod.makeCardPath;
 
 public class Wit extends AbstractDynamicCard {
@@ -18,6 +19,8 @@ public class Wit extends AbstractDynamicCard {
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+    public static final String UPGRADE_DESCRIPTION = languagePack.getCardStrings(ID).UPGRADE_DESCRIPTION;
+
 
     private static final int COST = 0;
     private static final int MAGIC_NUMBER = 1;
@@ -30,8 +33,10 @@ public class Wit extends AbstractDynamicCard {
 
     @Override
     public void upgrade() {
-        if(!upgraded) {
+        if (!upgraded) {
             upgradeName();
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
         }
     }
 
