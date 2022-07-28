0037b67f83b4db2949a0a32647165bae66fa6f99
18penning <8penning@informatik.uni-hamburg.de>
2022-07-28 19:42:03 +0200

    ShieldFace gives more Relay.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/ShieldFace.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/ShieldFace.java
@@ -23,7 +23,7 @@ public class ShieldFace extends AbstractDynamicCard {
     private static final int COST = 1;
     private static final int BLOCK = 6;
     private static final int UPGRADE_PLUS_BLOCK = 2;
-    private static final int MAGIC_NUMBER = 5;
+    private static final int MAGIC_NUMBER = 6;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 2;
 
     public ShieldFace() {
