f89e85ed8e497e873bc89a684b222d9a0880c201
18penning <8penning@informatik.uni-hamburg.de>
2022-07-28 16:19:34 +0200

    Suffocate now does not crash the game when playing.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/Suffocate.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Suffocate.java
@@ -15,7 +15,7 @@ public class Suffocate extends AbstractDynamicCard {
     public static final String ID = DefaultMod.makeID(Suffocate.class.getSimpleName());
     public static final String IMG = makeCardPath("Suffocate.png");
     private static final CardRarity RARITY = CardRarity.RARE;
-    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
