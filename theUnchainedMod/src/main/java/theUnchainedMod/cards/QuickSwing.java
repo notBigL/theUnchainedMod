0d4f7accf7c0914f72ea2829b3a6cc13223b5140
18penning <8penning@informatik.uni-hamburg.de>
2022-08-07 18:52:12 +0200

    QuickSwing does less damage now.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/QuickSwing.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/QuickSwing.java
@@ -24,8 +24,8 @@ public class QuickSwing extends AbstractDynamicCard {
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 8;
-    private static final int UPGRADE_PLUS_DMG = 3;
+    private static final int DAMAGE = 5;
+    private static final int UPGRADE_PLUS_DMG = 2;
     private static final int CHAIN_LENGTH = 1;
     private static final int MAGIC_NUMBER = 2;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
