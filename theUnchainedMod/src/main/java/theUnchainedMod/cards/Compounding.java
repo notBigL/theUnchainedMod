6fdbead5ef92d5822f0ab821ed40df9d35404f01
18penning <8penning@informatik.uni-hamburg.de>
2022-08-07 18:58:56 +0200

    Compounding gives Relay now.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/Compounding.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Compounding.java
@@ -1,5 +1,6 @@
 package theUnchainedMod.cards;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.unique.ArmamentsAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -8,6 +9,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theUnchainedMod.DefaultMod;
 import theUnchainedMod.actions.CompoundAction;
 import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.RelayPower;
 
 import static theUnchainedMod.DefaultMod.makeCardPath;
 
@@ -21,27 +23,27 @@ public class Compounding extends AbstractDynamicCard {
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
     private static final int COST = 2;
-    private static final int BLOCK = 9;
-    private static final int UPGRADE_PLUS_BLOCK = 3;
+    private static final int MAGIC_NUMBER = 12;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 4;
 
 
 
     public Compounding() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        baseBlock = block = BLOCK;
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
     }
 
     @Override
     public void upgrade() {
         if(!upgraded) {
             upgradeName();
-            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
         }
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, block));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new RelayPower(p, p, magicNumber)));
         AbstractDungeon.actionManager.addToBottom(new CompoundAction());
     }
 }
