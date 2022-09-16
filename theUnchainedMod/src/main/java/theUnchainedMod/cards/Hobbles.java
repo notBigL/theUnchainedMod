54dfe4e2e638390cb0e58090ddb5dfac1011b76e
18penning <8penning@informatik.uni-hamburg.de>
2022-09-16 21:59:51 +0200

    Hobbles now does more damage.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/Hobbles.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Hobbles.java
@@ -27,7 +27,7 @@ public class Hobbles extends AbstractDynamicCard {
     public static final String UPGRADE_DESCRIPTION = languagePack.getCardStrings(ID).UPGRADE_DESCRIPTION;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 7;
+    private static final int DAMAGE = 9;
     private static final int UPGRADE_PLUS_DMG = 3;
     private static final int MAGIC_NUMBER = 2;
     private static final int SECOND_MAGIC_NUMBER = 1;
