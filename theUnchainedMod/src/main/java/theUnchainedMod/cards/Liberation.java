ef25c56e6f7e31055f379243c9406a96b48f7b5e
18penning <8penning@informatik.uni-hamburg.de>
2022-09-21 00:58:37 +0200

    Liberation is common now.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/Liberation.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Liberation.java
@@ -13,7 +13,7 @@ public class Liberation extends AbstractDynamicCard {
 
     public static final String ID = DefaultMod.makeID(Liberation.class.getSimpleName());
     public static final String IMG = makeCardPath("Liberation.png");
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
