c5168d5975aa56fb5e82eae859f65382535c9c69
18penning <8penning@informatik.uni-hamburg.de>
2022-09-24 19:28:50 +0200

    New Pictures for the BasicDefend, Compunding, Riposte and Whiplash.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/Riposte.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Riposte.java
@@ -18,7 +18,7 @@ import static theUnchainedMod.DefaultMod.makeCardPath;
 public class Riposte extends AbstractDynamicCard {
 
     public static final String ID = DefaultMod.makeID(Riposte.class.getSimpleName());
-    public static final String IMG = makeCardPath("Swathe.png");
+    public static final String IMG = makeCardPath("Riposte.png");
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
