3b1d818f0ec611a201725b57c463a255412203ad
18penning <8penning@informatik.uni-hamburg.de>
2022-10-09 19:59:09 +0200

    Pirouette no longer gives a Swirl but instead more Momentum and upgrades all Swirls in hand now.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/PirouetteAction.java
@@ -0,0 +1,29 @@
+package theUnchainedMod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import theUnchainedMod.cards.Swirl;
+
+import java.util.Iterator;
+
+public class PirouetteAction extends AbstractGameAction {
+
+    @Override
+    public void update() {
+        Iterator var1;
+        AbstractCard c;
+
+                var1 = AbstractDungeon.player.hand.group.iterator();
+
+                while(var1.hasNext()) {
+                    c = (AbstractCard)var1.next();
+                    if (c instanceof Swirl && c.canUpgrade()) {
+                        c.upgrade();
+                        c.superFlash();
+                        c.applyPowers();
+                    }
+                }
+                this.isDone = true;
+    }
+}
--- theUnchainedMod/src/main/java/theUnchainedMod/cards/Pirouette.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/Pirouette.java
@@ -2,11 +2,13 @@ package theUnchainedMod.cards;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.WeakPower;
 import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.PirouetteAction;
 import theUnchainedMod.characters.TheDefault;
 import theUnchainedMod.patches.CustomTags;
 import theUnchainedMod.powers.MomentumPower;
@@ -26,14 +28,16 @@ public class Pirouette extends AbstractDynamicCard {
 
 
     private static final int COST = 0;
-    private static final int MAGIC_NUMBER = 1;
+    private static final int MAGIC_NUMBER = 4;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 2;
 
     public Pirouette() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseMagicNumber = magicNumber = MAGIC_NUMBER;
         tags.add(CustomTags.MOMENTUM);
-        this.cardsToPreview = new Swirl();
+        AbstractCard previewCard = new Swirl();
+        previewCard.upgrade();
+        this.cardsToPreview = previewCard;
         this.exhaust = true;
     }
 
@@ -47,11 +51,7 @@ public class Pirouette extends AbstractDynamicCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Swirl card = new Swirl();
-        if (p.hasPower("theUnchainedMod:FullSpinPower")) {
-            card.fullSpinApply(p.getPower("theUnchainedMod:FullSpinPower").amount);
-        }
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new MomentumPower(p, magicNumber)));
-        AbstractDungeon.actionManager.addToBottom(new MakeTempCardInHandAction(card));
+        AbstractDungeon.actionManager.addToBottom(new PirouetteAction());
     }
 }
