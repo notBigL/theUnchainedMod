ebef1ae7c90e69c22283df68282d8ea189820adf
18penning <8penning@informatik.uni-hamburg.de>
2022-10-21 18:49:14 +0200

    Nerf for DualBarrier.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/DualBarrier.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/DualBarrier.java
@@ -22,7 +22,7 @@ public class DualBarrier extends AbstractDynamicCard {
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
     private static final int COST = 1;
-    private static final int BLOCK = 7;
+    private static final int BLOCK = 3;
     private static final int UPGRADE_PLUS_BLOCK = 2;
     private static final int MAGIC_NUMBER = 8;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 3;
