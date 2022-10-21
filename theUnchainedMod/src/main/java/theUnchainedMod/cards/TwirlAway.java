6bff3ca15e59b6d70866816f74c7465ec10e3f56
18penning <8penning@informatik.uni-hamburg.de>
2022-10-21 17:08:39 +0200

    RoutineDodge is no more. Added a new Card called TwirlAway and its Power.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/TwirlAway.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/TwirlAway.java
@@ -1,30 +1,36 @@
 package theUnchainedMod.cards;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.NextTurnBlockPower;
 import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.GainMomentumAction;
 import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.GainMomentumNextTurnPower;
 
 import static theUnchainedMod.DefaultMod.makeCardPath;
 
-public class RoutineDodge extends AbstractDynamicCard {
+public class TwirlAway extends AbstractDynamicCard {
 
-    public static final String ID = DefaultMod.makeID(RoutineDodge.class.getSimpleName());
+    public static final String ID = DefaultMod.makeID(TwirlAway.class.getSimpleName());
     public static final String IMG = makeCardPath("RoutineDodge.png");
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
-    private static final int COST = 1;
-    private static final int BLOCK = 8;
+    private static final int COST = 2;
+    private static final int BLOCK = 10;
     private static final int UPGRADE_PLUS_BLOCK = 3;
+    private static final int MAGIC_NUMBER = 2;
 
-    public RoutineDodge() {
+    public TwirlAway() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseBlock = block = BLOCK;
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
     }
 
     @Override
@@ -38,5 +44,8 @@ public class RoutineDodge extends AbstractDynamicCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, this.block));
+        AbstractDungeon.actionManager.addToBottom(new GainMomentumAction(magicNumber));
+        //AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new NextTurnBlockPower(p, this.block), this.block));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new GainMomentumNextTurnPower(p, magicNumber, p)));
     }
 }
