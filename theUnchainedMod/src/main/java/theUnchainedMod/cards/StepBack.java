f59cb1a62fcd177334b90e9bab527da1b4327caa
18penning <8penning@informatik.uni-hamburg.de>
2022-10-22 18:21:29 +0200

    StepBack is now common, WindUp is now uncommon.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/StepBack.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/StepBack.java
@@ -17,7 +17,7 @@ public class StepBack extends AbstractDynamicCard {
 
     public static final String ID = DefaultMod.makeID(StepBack.class.getSimpleName());
     public static final String IMG = makeCardPath("StepBack.png");
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
