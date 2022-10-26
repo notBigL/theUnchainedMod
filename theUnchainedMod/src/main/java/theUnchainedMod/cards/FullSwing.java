7e3281edff123084383ff6b878bb8cd2ebb0c807
18penning <8penning@informatik.uni-hamburg.de>
2022-10-26 15:17:15 +0200

    Both of FullSwings instances of damage now work correctly with strength and vulnerable etc.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/FullSwing.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/FullSwing.java
@@ -1,5 +1,6 @@
 package theUnchainedMod.cards;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
@@ -7,10 +8,14 @@ import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
 import theUnchainedMod.DefaultMod;
 import theUnchainedMod.actions.GainMomentumAction;
 import theUnchainedMod.characters.TheDefault;
 import theUnchainedMod.patches.CustomTags;
+
+import java.util.Iterator;
 
 import static theUnchainedMod.DefaultMod.makeCardPath;
 
@@ -24,13 +29,15 @@ public class FullSwing extends AbstractDynamicCard {
 
     private static final int COST = 1;
     private static final int DAMAGE = 5;
-    private static final int MAGIC_NUMBER = 1;
-    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
+    private static final int MAGIC_NUMBER = 5;
+    private static final int SECOND_MAGIC_NUMBER = 1;
+    private static final int UPGRADE_PLUS_SECOND_MAGIC_NUMBER = 1;
 
     public FullSwing() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
         baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
         tags.add(CustomTags.SWING);
         tags.add(CustomTags.MOMENTUM);
         this.cardsToPreview = new Swirl();
@@ -41,15 +48,107 @@ public class FullSwing extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
+            upgradeDefaultSecondMagicNumber(UPGRADE_PLUS_SECOND_MAGIC_NUMBER);
+        }
+    }
+
+    @Override
+    public void applyPowers() {
+        super.applyPowers();
+        this.isMagicNumberModified = false;
+        AbstractPlayer player = AbstractDungeon.player;
+        float tmp = (float) this.baseMagicNumber;
+        Iterator var3 = player.relics.iterator();
+
+        while (var3.hasNext()) {
+            AbstractRelic r = (AbstractRelic) var3.next();
+            tmp = r.atDamageModify(tmp, this);
+            if (this.baseMagicNumber != (int) tmp) {
+                this.isMagicNumberModified = true;
+            }
         }
 
+        AbstractPower p;
+        for (var3 = player.powers.iterator(); var3.hasNext(); tmp = p.atDamageGive(tmp, this.damageTypeForTurn, this)) {
+            p = (AbstractPower) var3.next();
+        }
+
+        tmp = player.stance.atDamageGive(tmp, this.damageTypeForTurn, this);
+        if (this.baseMagicNumber != (int) tmp) {
+            this.isMagicNumberModified = true;
+        }
+
+        for (var3 = player.powers.iterator(); var3.hasNext(); tmp = p.atDamageFinalGive(tmp, this.damageTypeForTurn, this)) {
+            p = (AbstractPower) var3.next();
+        }
+
+        if (tmp < 0.0F) {
+            tmp = 0.0F;
+        }
+
+        if (this.baseMagicNumber != MathUtils.floor(tmp)) {
+            this.isMagicNumberModified = true;
+        }
+
+        this.magicNumber = MathUtils.floor(tmp);
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        super.calculateCardDamage(mo);
+
+        this.isMagicNumberModified = false;
+        AbstractPlayer player = AbstractDungeon.player;
+        if (mo != null) {
+            float tmp = (float) this.baseMagicNumber;
+            Iterator var9 = player.relics.iterator();
+
+            while (var9.hasNext()) {
+                AbstractRelic r = (AbstractRelic) var9.next();
+                tmp = r.atDamageModify(tmp, this);
+                if (this.baseMagicNumber != (int) tmp) {
+                    this.isMagicNumberModified = true;
+                }
+            }
+
+            AbstractPower p;
+            for (var9 = player.powers.iterator(); var9.hasNext(); tmp = p.atDamageGive(tmp, this.damageTypeForTurn, this)) {
+                p = (AbstractPower) var9.next();
+            }
+
+            tmp = player.stance.atDamageGive(tmp, this.damageTypeForTurn, this);
+            if (this.baseMagicNumber != (int) tmp) {
+                this.isMagicNumberModified = true;
+            }
+
+            for (var9 = mo.powers.iterator(); var9.hasNext(); tmp = p.atDamageReceive(tmp, this.damageTypeForTurn, this)) {
+                p = (AbstractPower) var9.next();
+            }
+
+            for (var9 = player.powers.iterator(); var9.hasNext(); tmp = p.atDamageFinalGive(tmp, this.damageTypeForTurn, this)) {
+                p = (AbstractPower) var9.next();
+            }
+
+            for (var9 = mo.powers.iterator(); var9.hasNext(); tmp = p.atDamageFinalReceive(tmp, this.damageTypeForTurn, this)) {
+                p = (AbstractPower) var9.next();
+            }
+
+            if (tmp < 0.0F) {
+                tmp = 0.0F;
+            }
+
+            if (this.baseMagicNumber != MathUtils.floor(tmp)) {
+                this.isMagicNumberModified = true;
+            }
+
+            this.magicNumber = MathUtils.floor(tmp);
+        }
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.magicNumber, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
         AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-        AbstractDungeon.actionManager.addToBottom(new GainMomentumAction(magicNumber));
+        AbstractDungeon.actionManager.addToBottom(new GainMomentumAction(defaultSecondMagicNumber));
     }
 }
