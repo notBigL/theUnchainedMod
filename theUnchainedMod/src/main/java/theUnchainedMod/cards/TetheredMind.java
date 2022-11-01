6dad3a47c1b88bf17d09d4e6ee8ba0bb7fa95683
18penning <8penning@informatik.uni-hamburg.de>
2022-11-01 16:43:25 +0100

    SoulConjunction.java is now called TetheredMind.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/TetheredMind.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/TetheredMind.java
@@ -9,16 +9,15 @@ import com.megacrit.cardcrawl.vfx.ThoughtBubble;
 import theUnchainedMod.DefaultMod;
 import theUnchainedMod.actions.GainRelayAction;
 import theUnchainedMod.characters.TheDefault;
-import theUnchainedMod.powers.RelayPower;
 import theUnchainedMod.powers.TiedToAnEnemyPower;
 import theUnchainedMod.powers.TiedToThePlayerPower;
 
 import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
 import static theUnchainedMod.DefaultMod.makeCardPath;
 
-public class SoulConjunction extends AbstractDynamicCard {
+public class TetheredMind extends AbstractDynamicCard {
 
-    public static final String ID = DefaultMod.makeID(SoulConjunction.class.getSimpleName());
+    public static final String ID = DefaultMod.makeID(TetheredMind.class.getSimpleName());
     public static final String IMG = makeCardPath("TieTogether.png");
     private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.SELF_AND_ENEMY;
@@ -29,7 +28,7 @@ public class SoulConjunction extends AbstractDynamicCard {
     private static final int MAGIC_NUMBER = 10;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 4;
 
-    public SoulConjunction() {
+    public TetheredMind() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseMagicNumber = magicNumber = MAGIC_NUMBER;
         this.exhaust = true;
