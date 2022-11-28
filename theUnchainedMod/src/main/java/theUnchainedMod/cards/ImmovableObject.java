75c55b1c9e5a2fff544f3e3ef0eb6c6b4d0a9b68
18penning <8penning@informatik.uni-hamburg.de>
2022-11-28 23:10:07 +0100

    TargetJoints is now ImmovableObject. Also added a new Power that ImmovableObject needs.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/ImmovableObject.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/ImmovableObject.java
@@ -1,33 +1,37 @@
 package theUnchainedMod.cards;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theUnchainedMod.DefaultMod;
-import theUnchainedMod.actions.ApplyCrushedArmorAction;
-import theUnchainedMod.actions.TargetJointsAction;
 import theUnchainedMod.characters.TheDefault;
+import theUnchainedMod.powers.ImmovableObjectPower;
 
 import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
 import static theUnchainedMod.DefaultMod.makeCardPath;
 
-public class TargetJoints extends AbstractDynamicCard {
+public class ImmovableObject extends AbstractDynamicCard {
 
-    public static final String ID = DefaultMod.makeID(TargetJoints.class.getSimpleName());
+    public static final String ID = DefaultMod.makeID(ImmovableObject.class.getSimpleName());
     public static final String IMG = makeCardPath("TargetJoints.png");
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
     public static final String UPGRADE_DESCRIPTION = languagePack.getCardStrings(ID).UPGRADE_DESCRIPTION;
 
 
-    private static final int COST = 1;
-    private static final int MAGIC_NUMBER = 2;
+    private static final int COST = 2;
+    private static final int BLOCK = 11;
+    private static final int UPGRADE_PLUS_BLOCK = 2;
+    private static final int MAGIC_NUMBER = 3;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
 
-    public TargetJoints() {
+    public ImmovableObject() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = block = BLOCK;
         baseMagicNumber = magicNumber = MAGIC_NUMBER;
     }
 
@@ -35,13 +39,14 @@ public class TargetJoints extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
             upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
         }
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToTop(new ApplyCrushedArmorAction(m, magicNumber));
-        AbstractDungeon.actionManager.addToBottom(new TargetJointsAction(m, magicNumber));
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, block));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new ImmovableObjectPower(p, magicNumber)));
     }
 }
---
+++ theUnchainedMod/src/main/java/theUnchainedMod/powers/ImmovableObjectPower.java
@@ -0,0 +1,56 @@
+package theUnchainedMod.powers;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.ApplyCrushedArmorAction;
+import theUnchainedMod.util.TextureLoader;
+
+public class ImmovableObjectPower extends AbstractPower {
+
+    public static final String POWER_ID = DefaultMod.makeID("ImmovableObjectPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    private static final Texture texture48 = TextureLoader.getTexture("theUnchainedModResources/images/powers/ImmovableObjectPower_power48.png");
+    private static final Texture texture128 = TextureLoader.getTexture("theUnchainedModResources/images/powers/ImmovableObjectPower_power128.png");
+
+    public ImmovableObjectPower(final AbstractCreature owner, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        type = PowerType.BUFF;
+        isTurnBased = false;
+
+        this.region128 = new TextureAtlas.AtlasRegion(texture128, 0, 0, 128, 128);
+        this.region48 = new TextureAtlas.AtlasRegion(texture48, 0, 0, 48, 48);
+
+        updateDescription();
+    }
+
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+    }
+
+    public void atStartOfTurn() {
+        AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(owner, owner, this));
+    }
+
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        if (info.owner != null && info.type != DamageInfo.DamageType.THORNS && info.type != DamageInfo.DamageType.HP_LOSS && info.owner != this.owner) {
+            this.flash();
+            AbstractDungeon.actionManager.addToTop(new ApplyCrushedArmorAction(info.owner, amount));
+        }
+
+        return damageAmount;
+    }
+}
