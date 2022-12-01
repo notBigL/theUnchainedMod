7c802761c2c144a4cc831fc82f15f93c9c641b40
18penning <8penning@informatik.uni-hamburg.de>
2022-12-01 23:17:18 +0100

    ReinforcedLink is now uncommon.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/ReinforcedLink.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/ReinforcedLink.java
@@ -19,7 +19,7 @@ public class ReinforcedLink extends AbstractDynamicCard {
 
     public static final String ID = DefaultMod.makeID(ReinforcedLink.class.getSimpleName());
     public static final String IMG = makeCardPath("ReinforcedLink.png");
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
