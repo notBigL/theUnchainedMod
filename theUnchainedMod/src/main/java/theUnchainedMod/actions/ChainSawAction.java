78266874f61cedff388e7af59bc55be59bea2068
18penning <8penning@informatik.uni-hamburg.de>
2022-12-04 21:33:46 +0100

    ChainSaw rework.

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/ChainSawAction.java
@@ -0,0 +1,36 @@
+package theUnchainedMod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ModifyDamageAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.GetAllInBattleInstances;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import theUnchainedMod.powers.AbstractChainPower;
+
+import java.util.Iterator;
+import java.util.UUID;
+
+public class ChainSawAction extends AbstractGameAction {
+
+    private final UUID cardID;
+
+    public ChainSawAction(UUID uuid, int amountPerChain) {
+        this.amount = amountPerChain;
+        this.cardID = uuid;
+    }
+
+    @Override
+    public void update() {
+        int amountToAdd = 0;
+
+        for (AbstractPower abstractPower : AbstractDungeon.player.powers) {
+            if (abstractPower instanceof AbstractChainPower) {
+                amountToAdd += amount;
+            }
+        }
+        if (amountToAdd > 0) {
+            AbstractDungeon.actionManager.addToBottom(new ModifyDamageAction(cardID, amountToAdd));
+        }
+        isDone = true;
+    }
+}
--- theUnchainedMod/src/main/java/theUnchainedMod/cards/ChainSaw.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/ChainSaw.java
@@ -7,6 +7,7 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.ChainSawAction;
 import theUnchainedMod.characters.TheUnchained;
 
 import static theUnchainedMod.DefaultMod.makeCardPath;
@@ -22,24 +23,27 @@ public class ChainSaw extends AbstractDynamicCard {
 
     private static final int COST = 1;
     private static final int DAMAGE = 15;
-    private static final int UPGRADE_PLUS_DMG = 5;
+    private static final int MAGIC_NUMBER = 5;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 5;
 
     public ChainSaw() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
     }
 
     @Override
     public void upgrade() {
-        if(!upgraded) {
+        if (!upgraded) {
             upgradeName();
-            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
         }
 
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ChainSawAction(this.uuid, magicNumber));
         AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HEAVY));
     }
 }
