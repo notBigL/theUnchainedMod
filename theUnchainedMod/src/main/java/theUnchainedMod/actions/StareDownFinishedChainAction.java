d05652ca5ec281e4e86a861e87041365317d18fe
18penning <8penning@informatik.uni-hamburg.de>
2022-12-18 15:35:49 +0100

    StareDownFinishedChainAction now shows the effects Eye on the enemies, instead of the Player.

--- theUnchainedMod/src/main/java/theUnchainedMod/actions/StareDownFinishedChainAction.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/actions/StareDownFinishedChainAction.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.WeakPower;
@@ -18,9 +19,11 @@ public class StareDownFinishedChainAction extends AbstractGameAction {
 
     @Override
     public void update() {
-        AbstractDungeon.actionManager.addToBottom(new VFXAction(new GiantEyeEffect(AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY, new Color(0.87F, 0.83F, 0.18F, 1.0F))));
         for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
-            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, AbstractDungeon.player, new WeakPower(mo, this.amount, false), this.amount, true, AttackEffect.NONE));
+            if (!mo.isDeadOrEscaped()) {
+                AbstractDungeon.actionManager.addToBottom(new VFXAction(new GiantEyeEffect(mo.hb.cX, mo.hb.cY, new Color(0.87F, 0.83F, 0.18F, 1.0F))));
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, AbstractDungeon.player, new WeakPower(mo, this.amount, false), this.amount, true, AttackEffect.NONE));
+            }
         }
         this.isDone = true;
     }
