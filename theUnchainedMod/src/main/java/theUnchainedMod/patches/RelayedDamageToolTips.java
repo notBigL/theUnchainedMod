41da01e7f7d09903e6548a8e16aa988356d2c893
18penning <8penning@informatik.uni-hamburg.de>
2023-01-12 00:43:59 +0100

    Added Relay to the Tooltips when hovering the player.

--- theUnchainedMod/src/main/java/theUnchainedMod/patches/RelayedDamageToolTips.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/patches/RelayedDamageToolTips.java
@@ -18,6 +18,10 @@ import java.util.ArrayList;
 @SpirePatch(clz = AbstractPlayer.class, method = "renderPowerTips")
 public class RelayedDamageToolTips {
 
+    private static final PowerStrings relayStrings = CardCrawlGame.languagePack.getPowerStrings("theUnchainedMod:RelayPower");
+    private static final String[] RELAY_DESCRIPTIONS = relayStrings.DESCRIPTIONS;
+    private static final Texture relayTexture = TextureLoader.getTexture("theUnchainedModResources/images/ui/RelayTooltipIcon.png");
+    private static final TextureAtlas.AtlasRegion relayTexture48 = new TextureAtlas.AtlasRegion(relayTexture, 0, 0, 48, 48);
     private static final PowerStrings nextTurnRelayedDamageStrings = CardCrawlGame.languagePack.getPowerStrings("theUnchainedMod:NextTurnRelayedDamage");
     private static final String[] NEXT_TURN_DESCRIPTIONS = nextTurnRelayedDamageStrings.DESCRIPTIONS;
     private static final Texture nextTurnTexture = TextureLoader.getTexture("theUnchainedModResources/images/ui/NextTurnRelayedDamageUIIcon_Centered.png");
@@ -30,6 +34,10 @@ public class RelayedDamageToolTips {
 
     @SpireInsertPatch(rloc = 1, localvars = {"tips"})
     public static void insertRelayedDamageToolTips(AbstractPlayer player, SpriteBatch sb, @ByRef ArrayList<PowerTip>[] tips) {
+        if(RelayHelpers.currentRelay.get(player) > 0) {
+            String description = RELAY_DESCRIPTIONS[0] + RelayHelpers.currentRelay.get(player) + RELAY_DESCRIPTIONS[1];
+            tips[0].add(new PowerTip(relayStrings.NAME, description, relayTexture48));
+        }
         if (RelayHelpers.nextTurnRelayedDamage.get(player) > 0) {
             String description = NEXT_TURN_DESCRIPTIONS[0] + RelayHelpers.nextTurnRelayedDamage.get(player) + NEXT_TURN_DESCRIPTIONS[1] + RelayHelpers.nextTurnRelayedDamage.get(player) / 2 + NEXT_TURN_DESCRIPTIONS[2];
             tips[0].add(new PowerTip(nextTurnRelayedDamageStrings.NAME, description, nextTurnTexture48));
