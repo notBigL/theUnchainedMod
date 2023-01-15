06894a174637cd14f50a1537365f3af918a0b2f2
18penning <8penning@informatik.uni-hamburg.de>
2023-01-15 18:00:35 +0100

    Eye for an eye now gives innate vigor.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/EyeForAnEye.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/EyeForAnEye.java
@@ -1,8 +1,10 @@
 package theUnchainedMod.cards;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 import theUnchainedMod.DefaultMod;
 import theUnchainedMod.actions.EyeForAnEyeAction;
 import theUnchainedMod.characters.TheUnchained;
@@ -18,17 +20,19 @@ public class EyeForAnEye extends AbstractDynamicCard {
     public static final CardColor COLOR = TheUnchained.Enums.COLOR_ORANGE;
 
     private static final int COST = 1;
-    private static final int MAGIC_NUMBER = 12;
-    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 6;
+    private static final int MAGIC_NUMBER = 10;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 5;
+    private static final int SECOND_MAGIC_NUMBER = 5;
 
     public EyeForAnEye() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        defaultBaseSecondMagicNumber = defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
     }
 
     @Override
     public void upgrade() {
-        if(!upgraded) {
+        if (!upgraded) {
             upgradeName();
             upgradeMagicNumber(UPGRADE_PLUS_MAGIC_NUMBER);
         }
@@ -36,6 +40,7 @@ public class EyeForAnEye extends AbstractDynamicCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new VigorPower(p, defaultSecondMagicNumber)));
         AbstractDungeon.actionManager.addToBottom(new EyeForAnEyeAction(magicNumber, p));
     }
 }
