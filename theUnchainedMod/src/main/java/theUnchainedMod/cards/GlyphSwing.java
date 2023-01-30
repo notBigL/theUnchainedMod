7b27cf6ac9db16dc45e185fef7bc5263167e9957
18penning <8penning@informatik.uni-hamburg.de>
2023-01-30 14:46:11 +0100

    GlyphSwing+ buff.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/GlyphSwing.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/GlyphSwing.java
@@ -25,10 +25,10 @@ public class GlyphSwing extends AbstractDynamicCard {
     public static final CardColor COLOR = TheUnchained.Enums.COLOR_ORANGE;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 5;
+    private static final int DAMAGE = 6;
     private static final int UPGRADE_PLUS_DMG = 2;
     private static final int MAGIC_NUMBER = 8;
-    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 3;
+    private static final int UPGRADE_PLUS_MAGIC_NUMBER = 4;
 
     public GlyphSwing() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
