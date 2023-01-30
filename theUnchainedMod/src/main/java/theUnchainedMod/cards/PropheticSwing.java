9935ec28fef68ec3e24613e914c0e061692873ed
18penning <8penning@informatik.uni-hamburg.de>
2023-01-30 14:15:31 +0100

    PropheticSwing is now common.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/PropheticSwing.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/PropheticSwing.java
@@ -18,14 +18,14 @@ public class PropheticSwing extends AbstractDynamicCard {
 
     public static final String ID = DefaultMod.makeID(PropheticSwing.class.getSimpleName());
     public static final String IMG = makeCardPath("PropheticSwing.png");
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheUnchained.Enums.COLOR_ORANGE;
 
 
     private static final int COST = 1;
-    private static final int DAMAGE = 11;
+    private static final int DAMAGE = 10;
     private static final int UPGRADE_PLUS_DMG = 3;
     private static final int MAGIC_NUMBER = 3;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
