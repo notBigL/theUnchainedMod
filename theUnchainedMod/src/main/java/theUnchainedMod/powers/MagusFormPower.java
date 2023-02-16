f0c36be9c2817f1e86d602ab2f448b221e757eff
18penning <8penning@informatik.uni-hamburg.de>
2023-02-16 17:23:20 +0100

    Fixed Power description of MagusFormPower

--- theUnchainedMod/src/main/java/theUnchainedMod/powers/MagusFormPower.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/powers/MagusFormPower.java
@@ -42,9 +42,9 @@ public class MagusFormPower extends AbstractPower {
 
     public void updateDescription() {
         if (this.amount == 1) {
-            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[2];
+            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
         } else {
-            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[3];
+            this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[2];
         }
     }
 
