9f306b92545e711c5f2c70a2df6a5cf06dea7f2a
18penning <8penning@informatik.uni-hamburg.de>
2023-02-23 15:56:49 +0100

    SingleOut nerf.

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/SingleOut.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/SingleOut.java
@@ -21,7 +21,7 @@ public class SingleOut extends AbstractDynamicCard {
     public static final CardColor COLOR = TheUnchained.Enums.COLOR_ORANGE;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 12;
+    private static final int DAMAGE = 11;
     private static final int UPGRADE_PLUS_DMG = 4;
     private static final int MAGIC_NUMBER = 3;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 1;
