3f3d7f551f4d6642c35308951aaddde04dc9fdc6
Felix Swimmer <mezteenx@googlemail.com>
2023-04-05 09:46:05 +0200

    added Make Haste

---
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/MakeHaste.java
@@ -0,0 +1,48 @@
+package theUnchainedMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.LoseStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import theUnchainedMod.DefaultMod;
+import theUnchainedMod.actions.MakeHasteAction;
+import theUnchainedMod.characters.TheUnchained;
+import theUnchainedMod.powers.HeavyHitPower;
+
+import static theUnchainedMod.DefaultMod.makeCardPath;
+
+public class MakeHaste extends AbstractDynamicCard{
+
+    public static final String ID = DefaultMod.makeID(MakeHaste.class.getSimpleName());
+    public static final String IMG = makeCardPath("MakeHaste.png");
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheUnchained.Enums.COLOR_ORANGE;
+
+    private static final int COST = 1;
+    private static final int MAGIC_NUMBER = 3;
+    private static final int SECOND_MAGIC_NUMBER = 2;
+
+
+    public MakeHaste() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC_NUMBER;
+        defaultBaseSecondMagicNumber= defaultSecondMagicNumber = SECOND_MAGIC_NUMBER;
+    }
+
+    @Override
+    public void upgrade() {
+        if(!upgraded) {
+            upgradeName();
+            upgradeBaseCost(0);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new MakeHasteAction());
+    }
+}
