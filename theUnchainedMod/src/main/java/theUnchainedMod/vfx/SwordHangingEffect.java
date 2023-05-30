46bf3d846378d78f7a643051519e4a89e69d23de
Felix Swimmer <mezteenx@googlemail.com>
2023-05-30 15:04:01 +0200

    finished sword of damocles effect

--- theUnchainedMod/src/main/java/theUnchainedMod/vfx/SwordHangingEffect.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/vfx/SwordHangingEffect.java
@@ -4,8 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Interpolation;
-import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 
@@ -15,30 +13,25 @@ import static theUnchainedMod.TheUnchainedMod.makeVFXPath;
 public class SwordHangingEffect extends AbstractGameEffect {
     private float x;
     private float y;
-    private float sX;
-    private float sY;
-    private float tX;
-    private float tY;
     private float scaleX;
     private float scaleY;
     private Color color2;
-    private final static Texture IMG = loadImage(makeVFXPath("Sword_of_Damocles_Effect.png"));
+    private final static Texture DamoclesGlyph = loadImage(makeVFXPath("Sword_of_Damocles_Effect_1.png"));
+    private final static Texture DamoclesSword = loadImage(makeVFXPath("Sword_of_Damocles_Effect_2.png"));
+    private float timeSinceSwinging;
+    private float swordRotation;
+    private float rotationFactor = 0.0333f;
 
-    public SwordHangingEffect(float x, float y, float dX, float dY, float angle, Color color1, Color color2) {
+    public SwordHangingEffect(float x, float y, float dX, float dY, Color color1, Color color2) {
         this.x = x - 64.0F - dX / 2.0F * Settings.scale;
         this.y = y - 64.0F - dY / 2.0F * Settings.scale;
-        this.sX = this.x;
-        this.sY = this.y;
-        this.tX = this.x + dX / 2.0F * Settings.scale;
-        this.tY = this.y + dY / 2.0F * Settings.scale;
         this.color = color1.cpy();
         this.color2 = color2.cpy();
         this.color.a = 0.0F;
-        this.startingDuration = 0.4F;
-        this.duration = this.startingDuration;
         this.scaleX = 0.01F;
         this.scaleY = 0.01F;
-        this.rotation = angle;
+        this.rotation = 0;
+        timeSinceSwinging = 0;
     }
 
     public void update() {
@@ -46,16 +39,21 @@ public class SwordHangingEffect extends AbstractGameEffect {
         this.scaleX = 1;
         this.scaleY = this.scaleX;
 
-        //this.duration -= Gdx.graphics.getDeltaTime();
-        if (this.duration < 0.0F) {
-            this.isDone = true;
-        }
+        timeSinceSwinging += Gdx.graphics.getDeltaTime() * 2;
+        swordRotation += Math.cos(timeSinceSwinging) * rotationFactor;
     }
 
     public void render(SpriteBatch sb) {
+        this.rotation = 0;
         sb.setColor(this.color2);
         sb.setBlendFunction(770, 1);
-        sb.draw(IMG, this.x - 15f, this.y, 64.0F, 64.0F, 128.0F, 368.0F, this.scaleX * Settings.scale, this.scaleY * Settings.scale, this.rotation, 0, 0, 128, 368, false, false);
+        sb.draw(DamoclesGlyph, this.x - 15f, this.y, 64.0F, 64.0F, 128.0F, 368.0F, this.scaleX * Settings.scale, this.scaleY * Settings.scale, this.rotation, 0, 0, 128, 368, false, false);
+
+        this.rotation = swordRotation;
+        sb.setColor(this.color2);
+        sb.setBlendFunction(770, 1);
+        sb.draw(DamoclesSword, this.x - 15f, this.y-48, 64.0F, 348.0F, 128.0F, 368.0F, this.scaleX * Settings.scale, this.scaleY * Settings.scale, this.rotation, 0, 0, 128, 368, false, false);
+
         sb.setBlendFunction(770, 771);
     }
 
--- theUnchainedMod/src/main/java/theUnchainedMod/vfx/SwordOfDamoclesEffect.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/vfx/SwordOfDamoclesEffect.java
@@ -20,7 +20,7 @@ public class SwordOfDamoclesEffect extends AbstractGameEffect {
     }
     public void update() {
         if(!SwordIsHanging) {
-            sword = new SwordHangingEffect(this.x, this.y - 30.0F * Settings.scale, 0.0F,-500.0F,0F, Color.GOLD, Color.GOLD);
+            sword = new SwordHangingEffect(this.x, this.y - 30.0F * Settings.scale, 0.0F,-500.0f, Color.GOLD, Color.GOLD);
             sword.isDone = false;
             AbstractDungeon.effectsQueue.add(sword); //TODO: this might not work? maybe its not actually creating the sword
             SwordIsHanging = true;
