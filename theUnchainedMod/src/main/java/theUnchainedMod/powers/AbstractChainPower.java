abdd2314f8859c33e841d7721430de4635c362f3
notBigL <99298254+notBigL@users.noreply.github.com>
2024-03-10 15:01:37 +0100

    code cleanup

--- theUnchainedMod/src/main/java/theUnchainedMod/powers/AbstractChainPower.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/powers/AbstractChainPower.java
@@ -9,7 +9,6 @@ import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
