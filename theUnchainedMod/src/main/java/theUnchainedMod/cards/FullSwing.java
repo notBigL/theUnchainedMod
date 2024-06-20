146e96d171ab6efd1dda90820d7427fa2ed17a72
notBigL <99298254+notBigL@users.noreply.github.com>
2024-06-20 17:35:08 +0200

    FullSwing Buff

--- theUnchainedMod/src/main/java/theUnchainedMod/cards/FullSwing.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/cards/FullSwing.java
@@ -30,9 +30,9 @@ public class FullSwing extends AbstractDynamicCard {
     public static final CardColor COLOR = TheUnchained.Enums.UNCHAINED_COLOR;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 5;
+    private static final int DAMAGE = 6;
     private static final int UPGRADE_PLUS_DAMAGE = 2;
-    private static final int MAGIC_NUMBER = 5;
+    private static final int MAGIC_NUMBER = 6;
     private static final int UPGRADE_PLUS_MAGIC_NUMBER = 2;
 
     public FullSwing() {
