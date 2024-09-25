3aeab0839eb96d5d394e299a97aa104b20150d23
notBigL <99298254+notBigL@users.noreply.github.com>
2024-09-25 12:13:40 +0200

    UI strings in character select screen are no longer hardcoded, and are instead in the UI strings json now.

--- theUnchainedMod/src/main/java/theUnchainedMod/patches/CharacterSelectUIPatch.java
+++ theUnchainedMod/src/main/java/theUnchainedMod/patches/CharacterSelectUIPatch.java
@@ -4,11 +4,13 @@ import basemod.BaseMod;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.helpers.Hitbox;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.helpers.input.InputHelper;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.screens.charSelect.CharacterOption;
 import com.megacrit.cardcrawl.screens.charSelect.CharacterSelectScreen;
 import theUnchainedMod.TheUnchainedMod;
@@ -16,13 +18,19 @@ import theUnchainedMod.characters.TheUnchained;
 import theUnchainedMod.relics.CrushingGauntlets;
 import theUnchainedMod.util.TextureLoader;
 
+
 public class CharacterSelectUIPatch {
+
+    public static final String[] birthdaySkinText;
+    public static final String[] unboundSkinText;
+    public static final String[] boosterPackText;
+
     private static float UI_TEXT_SCALE = 0.6f;
     private static int UI_MAX_LINE_WIDTH = 450;
     private static int UI_LINE_SPACING = 25;
     private static int SPACING_BETWEEN_BUTTONS = 40;
     private static int UI_BUTTON_DEFAULT_X = 170;
-    private static int UI_FIRST_BUTTON_DEFAULT_Y = 1080-700;
+    private static int UI_FIRST_BUTTON_DEFAULT_Y = 1080 - 700;
     private static int UI_CURRENT_BUTTON_Y = UI_FIRST_BUTTON_DEFAULT_Y;
 
 
@@ -48,17 +56,17 @@ public class CharacterSelectUIPatch {
     public static Hitbox booster_hitbox;
 
     @SpirePatch2(clz = CharacterSelectScreen.class, method = "open")
-    public static class OpenPatch{
-        public static void Prefix(){
+    public static class OpenPatch {
+        public static void Prefix() {
             UI_CURRENT_BUTTON_Y = UI_FIRST_BUTTON_DEFAULT_Y;
             birthday_hitbox = new Hitbox(birthday_XPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, birthday_width * Settings.scale, birthday_height * Settings.scale);
             UI_CURRENT_BUTTON_Y -= SPACING_BETWEEN_BUTTONS;
 
-            if(TheUnchainedMod.PrinceUnboundUnlocked() || TheUnchainedMod.ContentUnlocked()) {
+            if (TheUnchainedMod.PrinceUnboundUnlocked() || TheUnchainedMod.ContentUnlocked()) {
                 pu_hitbox = new Hitbox(pu_xPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, pu_width * Settings.scale, pu_height * Settings.scale);
                 UI_CURRENT_BUTTON_Y -= SPACING_BETWEEN_BUTTONS;
             }
-            if(TheUnchainedMod.BoosterpackUnlocked() || TheUnchainedMod.ContentUnlocked()) {
+            if (TheUnchainedMod.BoosterpackUnlocked() || TheUnchainedMod.ContentUnlocked()) {
                 booster_hitbox = new Hitbox(booster_xPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, booster_width * Settings.scale, booster_height * Settings.scale);
                 UI_CURRENT_BUTTON_Y -= SPACING_BETWEEN_BUTTONS;
             }
@@ -66,126 +74,131 @@ public class CharacterSelectUIPatch {
     }
 
     @SpirePatch2(clz = CharacterSelectScreen.class, method = "render")
-    public static class RenderPatch{
-        public static void Postfix(CharacterSelectScreen __instance, SpriteBatch sb){
+    public static class RenderPatch {
+        public static void Postfix(CharacterSelectScreen __instance, SpriteBatch sb) {
             UI_CURRENT_BUTTON_Y = UI_FIRST_BUTTON_DEFAULT_Y;
 
-                for(CharacterOption o : __instance.options){
-                    if(o.selected && o.c.chosenClass.equals(TheUnchained.Enums.THE_UNCHAINED)){
+            for (CharacterOption o : __instance.options) {
+                if (o.selected && o.c.chosenClass.equals(TheUnchained.Enums.THE_UNCHAINED)) {
 
-                        sb.draw(ImageMaster.CHECKBOX, birthday_XPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, birthday_width * Settings.scale, birthday_height * Settings.scale);
-                        if (birthdayButtonStatus) sb.draw(ImageMaster.TICK, birthday_XPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, birthday_width * Settings.scale, birthday_height * Settings.scale);
-                        FontHelper.renderSmartText(sb, FontHelper.menuBannerFont, "Activate Birthday Boy Mode.", (birthday_XPos + birthday_width + 5) * Settings.scale, (UI_CURRENT_BUTTON_Y + birthday_height) * Settings.scale, UI_MAX_LINE_WIDTH, UI_LINE_SPACING, (birthday_hitbox.hovered ? Color.GOLD : Color.WHITE), UI_TEXT_SCALE);
-                        birthday_hitbox.render(sb);
+                    sb.draw(ImageMaster.CHECKBOX, birthday_XPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, birthday_width * Settings.scale, birthday_height * Settings.scale);
+                    if (birthdayButtonStatus)
+                        sb.draw(ImageMaster.TICK, birthday_XPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, birthday_width * Settings.scale, birthday_height * Settings.scale);
+                    FontHelper.renderSmartText(sb, FontHelper.menuBannerFont, birthdaySkinText[0], (birthday_XPos + birthday_width + 5) * Settings.scale, (UI_CURRENT_BUTTON_Y + birthday_height) * Settings.scale, UI_MAX_LINE_WIDTH, UI_LINE_SPACING, (birthday_hitbox.hovered ? Color.GOLD : Color.WHITE), UI_TEXT_SCALE);
+                    birthday_hitbox.render(sb);
+                    UI_CURRENT_BUTTON_Y -= SPACING_BETWEEN_BUTTONS;
+
+                    if (TheUnchainedMod.PrinceUnboundUnlocked() || TheUnchainedMod.ContentUnlocked()) {
+                        sb.draw(ImageMaster.CHECKBOX, pu_xPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, pu_width * Settings.scale, pu_height * Settings.scale);
+                        if (princeUnboundButtonStatus)
+                            sb.draw(ImageMaster.TICK, pu_xPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, pu_width * Settings.scale, pu_height * Settings.scale);
+                        FontHelper.renderSmartText(sb, FontHelper.menuBannerFont, unboundSkinText[0], (pu_xPos + pu_width + 5) * Settings.scale, (UI_CURRENT_BUTTON_Y + pu_height) * Settings.scale, UI_MAX_LINE_WIDTH, UI_LINE_SPACING, (pu_hitbox.hovered ? Color.GOLD : Color.WHITE), UI_TEXT_SCALE);
+                        pu_hitbox.render(sb);
                         UI_CURRENT_BUTTON_Y -= SPACING_BETWEEN_BUTTONS;
-
-                        if(TheUnchainedMod.PrinceUnboundUnlocked() || TheUnchainedMod.ContentUnlocked())
-                        {
-                            sb.draw(ImageMaster.CHECKBOX, pu_xPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, pu_width * Settings.scale, pu_height * Settings.scale);
-                            if (princeUnboundButtonStatus) sb.draw(ImageMaster.TICK, pu_xPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, pu_width * Settings.scale, pu_height * Settings.scale);
-                            FontHelper.renderSmartText(sb, FontHelper.menuBannerFont, "Use the 'Prince Unbound' skin.", (pu_xPos + pu_width + 5) * Settings.scale, (UI_CURRENT_BUTTON_Y + pu_height) * Settings.scale, UI_MAX_LINE_WIDTH, UI_LINE_SPACING, (pu_hitbox.hovered ? Color.GOLD : Color.WHITE), UI_TEXT_SCALE);
-                            pu_hitbox.render(sb);
-                            UI_CURRENT_BUTTON_Y -= SPACING_BETWEEN_BUTTONS;
-                        }
-                        if(TheUnchainedMod.BoosterpackUnlocked() || TheUnchainedMod.ContentUnlocked())
-                        {
-                            sb.draw(ImageMaster.CHECKBOX, booster_xPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, booster_width * Settings.scale, booster_height * Settings.scale);
-                            if (boosterPackButtonStatus) sb.draw(ImageMaster.TICK, booster_xPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, booster_width * Settings.scale, booster_height * Settings.scale);
-                            FontHelper.renderSmartText(sb, FontHelper.menuBannerFont, "Activate the optional Booster Pack (Check Compendium for Cards and Relics).", (booster_xPos + booster_width + 5) * Settings.scale,(UI_CURRENT_BUTTON_Y + booster_height) * Settings.scale, UI_MAX_LINE_WIDTH, UI_LINE_SPACING, (booster_hitbox.hovered ? Color.GOLD : Color.WHITE), UI_TEXT_SCALE);
-                            booster_hitbox.render(sb);
-                            UI_CURRENT_BUTTON_Y -= SPACING_BETWEEN_BUTTONS;
-                        }
-                        break;
                     }
+                    if (TheUnchainedMod.BoosterpackUnlocked() || TheUnchainedMod.ContentUnlocked()) {
+                        sb.draw(ImageMaster.CHECKBOX, booster_xPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, booster_width * Settings.scale, booster_height * Settings.scale);
+                        if (boosterPackButtonStatus)
+                            sb.draw(ImageMaster.TICK, booster_xPos * Settings.scale, UI_CURRENT_BUTTON_Y * Settings.scale, booster_width * Settings.scale, booster_height * Settings.scale);
+                        FontHelper.renderSmartText(sb, FontHelper.menuBannerFont, boosterPackText[0], (booster_xPos + booster_width + 5) * Settings.scale, (UI_CURRENT_BUTTON_Y + booster_height) * Settings.scale, UI_MAX_LINE_WIDTH, UI_LINE_SPACING, (booster_hitbox.hovered ? Color.GOLD : Color.WHITE), UI_TEXT_SCALE);
+                        booster_hitbox.render(sb);
+                        UI_CURRENT_BUTTON_Y -= SPACING_BETWEEN_BUTTONS;
+                    }
+                    break;
                 }
             }
+        }
     }
 
 
     @SpirePatch2(clz = CharacterSelectScreen.class, method = "update")
-    public static class UpdatePatch{
-        public static void Postfix(CharacterSelectScreen __instance){
-                for(CharacterOption o : __instance.options){
-                    if(o.selected && o.c.chosenClass.equals(TheUnchained.Enums.THE_UNCHAINED)){
+    public static class UpdatePatch {
+        public static void Postfix(CharacterSelectScreen __instance) {
+            for (CharacterOption o : __instance.options) {
+                if (o.selected && o.c.chosenClass.equals(TheUnchained.Enums.THE_UNCHAINED)) {
 
-                        //  Birthday skin
+                    //  Birthday skin
 
-                        birthday_hitbox.update();
-                        if ((birthday_hitbox.hovered || birthday_hitbox.justHovered) && InputHelper.justClickedLeft) {
+                    birthday_hitbox.update();
+                    if ((birthday_hitbox.hovered || birthday_hitbox.justHovered) && InputHelper.justClickedLeft) {
+                        try {
+                            birthdayButtonStatus = !birthdayButtonStatus;
+                            TheUnchainedMod.unchainedConfig.setBool(TheUnchainedMod.BIRTHDAY_SKIN_ACTIVATED_PROPERTY, birthdayButtonStatus);
+
+                            //  turn off prince unbound skin button
+                            princeUnboundButtonStatus = false;
+                            TheUnchainedMod.unchainedConfig.setBool(TheUnchainedMod.PRINCE_UNBOUND_SKIN_ACTIVATED_PROPERTY, princeUnboundButtonStatus);
+
+                            __instance.bgCharImg = TextureLoader.getTexture(TheUnchainedMod.THE_DEFAULT_PORTRAIT);
+                            BaseMod.playerPortraitMap.put(TheUnchained.Enums.THE_UNCHAINED, TheUnchainedMod.THE_DEFAULT_PORTRAIT);
+
+                            TheUnchainedMod.unchainedConfig.save();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    //  Prince Unbound Skin
+
+                    if (TheUnchainedMod.PrinceUnboundUnlocked() || TheUnchainedMod.ContentUnlocked()) {
+                        pu_hitbox.update();
+                        if ((pu_hitbox.hovered || pu_hitbox.justHovered) && InputHelper.justClickedLeft) {
                             try {
-                                birthdayButtonStatus = !birthdayButtonStatus;
-                                TheUnchainedMod.unchainedConfig.setBool(TheUnchainedMod.BIRTHDAY_SKIN_ACTIVATED_PROPERTY, birthdayButtonStatus);
-
-                                //  turn off prince unbound skin button
-                                princeUnboundButtonStatus = false;
+                                princeUnboundButtonStatus = !princeUnboundButtonStatus;
                                 TheUnchainedMod.unchainedConfig.setBool(TheUnchainedMod.PRINCE_UNBOUND_SKIN_ACTIVATED_PROPERTY, princeUnboundButtonStatus);
 
-                                __instance.bgCharImg = TextureLoader.getTexture(TheUnchainedMod.THE_DEFAULT_PORTRAIT);
-                                BaseMod.playerPortraitMap.put(TheUnchained.Enums.THE_UNCHAINED, TheUnchainedMod.THE_DEFAULT_PORTRAIT);
+                                // turn off birthday skin button
+                                birthdayButtonStatus = false;
+                                TheUnchainedMod.unchainedConfig.setBool(TheUnchainedMod.BIRTHDAY_SKIN_ACTIVATED_PROPERTY, birthdayButtonStatus);
 
+                                if (princeUnboundButtonStatus) {
+                                    __instance.bgCharImg = TextureLoader.getTexture(TheUnchainedMod.PRINCE_UNBOUND_PORTRAIT);
+                                    BaseMod.playerPortraitMap.put(TheUnchained.Enums.THE_UNCHAINED, TheUnchainedMod.PRINCE_UNBOUND_PORTRAIT);
+
+                                } else {
+                                    __instance.bgCharImg = TextureLoader.getTexture(TheUnchainedMod.THE_DEFAULT_PORTRAIT);
+                                    BaseMod.playerPortraitMap.put(TheUnchained.Enums.THE_UNCHAINED, TheUnchainedMod.THE_DEFAULT_PORTRAIT);
+                                }
                                 TheUnchainedMod.unchainedConfig.save();
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
                         }
+                    }
 
-                        //  Prince Unbound Skin
+                    //  Booster Pack
 
-                        if(TheUnchainedMod.PrinceUnboundUnlocked() || TheUnchainedMod.ContentUnlocked()) {
-                            pu_hitbox.update();
-                            if ((pu_hitbox.hovered || pu_hitbox.justHovered) && InputHelper.justClickedLeft) {
-                                try {
-                                    princeUnboundButtonStatus = !princeUnboundButtonStatus;
-                                    TheUnchainedMod.unchainedConfig.setBool(TheUnchainedMod.PRINCE_UNBOUND_SKIN_ACTIVATED_PROPERTY, princeUnboundButtonStatus);
+                    if (TheUnchainedMod.BoosterpackUnlocked() || TheUnchainedMod.ContentUnlocked()) {
+                        booster_hitbox.update();
+                        if ((booster_hitbox.hovered || booster_hitbox.justHovered) && InputHelper.justClickedLeft) {
+                            try {
+                                boosterPackButtonStatus = !boosterPackButtonStatus;
+                                TheUnchainedMod.unchainedConfig.setBool(TheUnchainedMod.UNCHAINED_BOOSTER_PACK_ACTIVATED_PROPERTY, boosterPackButtonStatus);
 
-                                    // turn off birthday skin button
-                                    birthdayButtonStatus = false;
-                                    TheUnchainedMod.unchainedConfig.setBool(TheUnchainedMod.BIRTHDAY_SKIN_ACTIVATED_PROPERTY, birthdayButtonStatus);
-
-                                    if(princeUnboundButtonStatus) {
-                                        __instance.bgCharImg = TextureLoader.getTexture(TheUnchainedMod.PRINCE_UNBOUND_PORTRAIT);
-                                        BaseMod.playerPortraitMap.put(TheUnchained.Enums.THE_UNCHAINED, TheUnchainedMod.PRINCE_UNBOUND_PORTRAIT);
-
-                                    }
-                                    else
-                                    {
-                                        __instance.bgCharImg = TextureLoader.getTexture(TheUnchainedMod.THE_DEFAULT_PORTRAIT);
-                                        BaseMod.playerPortraitMap.put(TheUnchained.Enums.THE_UNCHAINED, TheUnchainedMod.THE_DEFAULT_PORTRAIT);
-                                    }
-                                    TheUnchainedMod.unchainedConfig.save();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
+                                if (boosterPackButtonStatus) {
+                                    if (!BaseMod.getRelicsInCustomPool(TheUnchained.Enums.UNCHAINED_COLOR).containsKey(CrushingGauntlets.ID))
+                                        BaseMod.addRelicToCustomPool(new CrushingGauntlets(), TheUnchained.Enums.UNCHAINED_COLOR);
+                                } else {
+                                    if (BaseMod.getRelicsInCustomPool(TheUnchained.Enums.UNCHAINED_COLOR).containsKey(CrushingGauntlets.ID))
+                                        BaseMod.removeRelicFromCustomPool(BaseMod.getRelicsInCustomPool(TheUnchained.Enums.UNCHAINED_COLOR).get(CrushingGauntlets.ID), TheUnchained.Enums.UNCHAINED_COLOR);
                                 }
+                                TheUnchainedMod.unchainedConfig.save();
+                            } catch (Exception e) {
+                                e.printStackTrace();
                             }
                         }
-
-                        //  Booster Pack
-
-                        if(TheUnchainedMod.BoosterpackUnlocked() || TheUnchainedMod.ContentUnlocked()) {
-                            booster_hitbox.update();
-                            if ((booster_hitbox.hovered || booster_hitbox.justHovered) && InputHelper.justClickedLeft) {
-                                try {
-                                    boosterPackButtonStatus = !boosterPackButtonStatus;
-                                    TheUnchainedMod.unchainedConfig.setBool(TheUnchainedMod.UNCHAINED_BOOSTER_PACK_ACTIVATED_PROPERTY, boosterPackButtonStatus);
-
-                                    if(boosterPackButtonStatus) {
-                                        if (!BaseMod.getRelicsInCustomPool(TheUnchained.Enums.UNCHAINED_COLOR).containsKey(CrushingGauntlets.ID))
-                                            BaseMod.addRelicToCustomPool(new CrushingGauntlets(), TheUnchained.Enums.UNCHAINED_COLOR);
-                                    }
-                                    else
-                                    {
-                                        if (BaseMod.getRelicsInCustomPool(TheUnchained.Enums.UNCHAINED_COLOR).containsKey(CrushingGauntlets.ID))
-                                            BaseMod.removeRelicFromCustomPool(BaseMod.getRelicsInCustomPool(TheUnchained.Enums.UNCHAINED_COLOR).get(CrushingGauntlets.ID), TheUnchained.Enums.UNCHAINED_COLOR);
-                                    }
-                                    TheUnchainedMod.unchainedConfig.save();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-                        break; //stop looking at new chars
+                    }
+                    break; //stop looking at new chars
                 }
             }
         }
     }
+
+    static {
+        birthdaySkinText = CardCrawlGame.languagePack.getUIString("theUnchainedMod:BirthdaySkin").TEXT;
+        unboundSkinText = CardCrawlGame.languagePack.getUIString("theUnchainedMod:UnboundSkin").TEXT;
+        boosterPackText = CardCrawlGame.languagePack.getUIString("theUnchainedMod:BoosterPack").TEXT;
+    }
 }
+
+
